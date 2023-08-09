d50ef9db8ebb8954edc2f2dfd19fb74e1cbeea8e
Ankit Khandelwal <akhandelwal2025@gmail.com>
2023-08-08 22:09:55 -0700

    finishing up some of the ros things

--- driverless_ws/src/planning/src/EKF_SLAM.cpp
+++ driverless_ws/src/planning/src/EKF_SLAM.cpp
@@ -10,12 +10,30 @@
 #include "eufs_msgs/msg/car_state.hpp"
 
 #include <boost/shared_ptr.hpp>
+#include <vector>
+#include <cmath>
+#include <chrono>
 
 #define CONE_DATA_TOPIC "/cones"
 #define VEHICLE_DATA_TOPIC "/ground_truth/state"
 
+using namespace std;
 using std::placeholders::_1;
 
+struct Cone{
+  double x;
+  double y;
+
+};
+
+struct VehiclePosition{
+  double x;
+  double y;
+  double yaw;
+  double dx;
+  double dy;
+  double dyaw;
+};
 class SLAMValidation : public rclcpp::Node
 {
   public:
@@ -24,17 +42,64 @@ class SLAMValidation : public rclcpp::Node
       "/cones", 10, std::bind(&SLAMValidation::cone_callback, this, _1));
       vehicle_state_sub = this->create_subscription<eufs_msgs::msg::CarState>(
       "/ground_truth/state", 10, std::bind(&SLAMValidation::vehicle_state_callback, this, _1));
+      timer = this->create_wall_timer(100ms, std::bind(&SLAMValidation::timer_callback, this));
+    }
+  private:
+    void cone_callback(const eufs_msgs::msg::ConeArrayWithCovariance::SharedPtr cone_data){
+      // RCLCPP_INFO(this->get_logger(), "B: %i| Y: %i| O: %i", cone_data->blue_cones.size(), cone_data->yellow_cones.size(), cone_data->orange_cones.size());
+      for(int i = 0; i < cone_data->blue_cones.size(); i++){
+        Cone to_add;
+        to_add.x = cone_data->blue_cones[i].point.x;
+        to_add.y = cone_data->blue_cones[i].point.y;
+        blue_cones.push_back(to_add);
+      }
+      for(int i = 0; i < cone_data->yellow_cones.size(); i++){
+        Cone to_add;
+        to_add.x = cone_data->yellow_cones[i].point.x;
+        to_add.y = cone_data->yellow_cones[i].point.y;
+        yellow_cones.push_back(to_add);
+      }
+      for(int i = 0; i < cone_data->orange_cones.size(); i++){
+        Cone to_add = {cone_data->orange_cones[i].point.x, cone_data->orange_cones[i].point.y};
+        orange_cones.push_back(to_add);
+      }
+    }
+    void vehicle_state_callback(const eufs_msgs::msg::CarState::SharedPtr vehicle_state_data){
+      // RCLCPP_INFO(this->get_logger(), "vehicle state\n");
+      double q1 = vehicle_state_data->pose.pose.orientation.x;
+      double q2 = vehicle_state_data->pose.pose.orientation.y;
+      double q3 = vehicle_state_data->pose.pose.orientation.z;
+      double q0 = vehicle_state_data->pose.pose.orientation.w;
+      double yaw = atan2(2*(q0*q3+q1*q2), pow(q0, 2)+pow(q1, 2)-pow(q2, 2)-pow(q3, 2));
+
+      vpos_is_jacob.x = vehicle_state_data->pose.pose.position.x;
+      vpos_is_jacob.y = vehicle_state_data->pose.pose.position.y;
+      vpos_is_jacob.yaw = yaw;
+      vpos_is_jacob.dx = vehicle_state_data->twist.twist.linear.x;
+      vpos_is_jacob.dy = vehicle_state_data->twist.twist.linear.y;
+      vpos_is_jacob.dyaw = vehicle_state_data->twist.twist.angular.z;
+    }
+    void timer_callback(){
+      run_slam(std::chrono::system_clock::now());
     }
 
-  private:
-    void cone_callback(const eufs_msgs::msg::ConeArrayWithCovariance::SharedPtr cone_data) const{
-      RCLCPP_INFO(this->get_logger(), "cones\n");
-    }
-    void vehicle_state_callback(const eufs_msgs::msg::CarState::SharedPtr vehicle_state_data) const{
-      RCLCPP_INFO(this->get_logger(), "vehicle state\n");
+    void run_slam(auto curr_time){
+      dt = std::chrono::duration_cast<std::chrono::microseconds>(curr_time - prev_time).count() / 1000000.0;
+      prev_time = curr_time;
+      RCLCPP_INFO(this->get_logger(), "curr_time: %d | prev_time: %d | dt: %d\n", curr_time.count(), prev_time.count(), dt);
+
     }
     rclcpp::Subscription<eufs_msgs::msg::ConeArrayWithCovariance>::SharedPtr cone_sub;
     rclcpp::Subscription<eufs_msgs::msg::CarState>::SharedPtr vehicle_state_sub;
+    vector<Cone> blue_cones;
+    vector<Cone> yellow_cones;
+    vector<Cone> orange_cones;
+
+    VehiclePosition vpos_is_jacob;
+    rclcpp::TimerBase::SharedPtr timer;
+
+    auto prev_time;
+    double dt;
 };
 
 // class SLAMValidation : public rclcpp::Node {
@@ -71,7 +136,7 @@ class SLAMValidation : public rclcpp::Node
 //                   sizeof(cone_data->orange_cones)/sizeof(cone_data->orange_cones[0]));
 //       RCLCPP_INFO(this->get_logger(), 
 //                   "X: %f, Y: %f, Z: %f\n", 
-//                   vehicle_state_data->pose.pose.position.x,
+//                   vehicle_state_data->pose.x,
 //                   vehicle_state_data->pose.pose.position.y,
 //                   vehicle_state_data->pose.pose.position.z);
 //       RCLCPP_INFO(this->get_logger(),
