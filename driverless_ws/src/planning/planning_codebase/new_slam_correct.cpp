d9c515816aa047c419f63a1399d83b1d5528093d
yuvanshu <yuvanshu.agarwal21@gmail.com>
2023-10-07 19:10:57 +0000

    added jacob_motion, calc_landmark_position, and get_landmark_position_from_state functions

--- driverless_ws/src/planning/planning_codebase/new_slam_correct.cpp
+++ driverless_ws/src/planning/planning_codebase/new_slam_correct.cpp
@@ -18,42 +18,47 @@ double STATE_SIZE = 3
 double LM_SIZE = 2
 
 // Create Q_sim matrix
-Eigen::Matrix2d Q_sim;
-Q_sim << 0.2 * 0.2, 0.0, 0.0, (Eigen::deg2rad(1.0) * Eigen::deg2rad(1.0));
+Eigen::MatrixXd Q_sim(2, 2);
+Q_sim << 0.2 * 0.2, 0.0, 
+         0.0, (Eigen::deg2rad(1.0) * Eigen::deg2rad(1.0));
 
 // Create R_sim matrix
-Eigen::Matrix2d R_sim;
-R_sim << 1.0 * 1.0, 0.0, 0.0, (Eigen::deg2rad(10.0) * Eigen::deg2rad(10.0));
+Eigen::MatrixXd R_sim(2,2);
+R_sim << 1.0 * 1.0, 0.0, 
+         0.0, (Eigen::deg2rad(10.0) * Eigen::deg2rad(10.0));
 
 // Create Cx matrix
-Eigen::Matrix3d Cx;
-Cx << 0.5 * 0.5, 0.0, 0.0, 0.0, 0.5 * 0.5, 0.0, 0.0, (Eigen::deg2rad(30.0) * Eigen::deg2rad(30.0))
+Eigen::MatrixXd Cx(3, 3);
+Cx << 0.5 * 0.5, 0.0, 0.0, 
+      0.0, 0.5 * 0.5, 0.0, 
+      0.0, 0.0, (Eigen::deg2rad(30.0) * Eigen::deg2rad(30.0));
 
 // Create alphas
-Eigen::VectorXd alphas(6);
+Eigen::MatrixXd alphas(6, 1);
 alphas << 0.11, 0.01, 0.18, 0.08, 0.0, 0.0;
 
 
-Eigen::Vector2d calcInput() {
+Eigen::MatrixXd calcInput() {
     // Define v and yaw_rate
     double v = 1.0;         // [m/s]
     double yaw_rate = 0.1;  // [rad/s]
 
     // Create a 2x1 column vector u
-    Eigen::Vector2d u;
-    u << v, yaw_rate;
+    Eigen::MatrixXd u(2, 1);
+    u << v, 
+         yaw_rate;
 
     return u;
 }
 
 // Function to calculate the motion model
-Eigen::VectorXd motion_model(const Eigen::VectorXd& x, const Eigen::Vector2d& u, double dt) {
-    Eigen::Matrix3d F;
+Eigen::MatrixXd motion_model(const Eigen::MatrixXd& x, const Eigen::MatrixXd& u, double dt) {
+    Eigen::MatrixXd F(3, 3);
     F << 1.0, 0.0, 0.0,
          0.0, 1.0, 0.0,
          0.0, 0.0, 1.0;
 
-    Eigen::Matrix<double, 3, 2> B;
+    Eigen::MatrixXd B(3, 2);
     B << dt * std::cos(x(2, 0)), 0.0,
          dt * std::sin(x(2, 0)), 0.0,
          0.0, dt;
@@ -62,15 +67,84 @@ Eigen::VectorXd motion_model(const Eigen::VectorXd& x, const Eigen::Vector2d& u,
 }
 
 // Function to calculate the number of landmarks
-int calc_n_lm(const Eigen::VectorXd& x) {
-    int n = static_cast<int>((x.size() - STATE_SIZE) / LM_SIZE);
+int calc_n_lm(const Eigen::MatrixXd& x) {
+    int n = static_cast<int>((x.rows() - STATE_SIZE) / LM_SIZE);
     return n;
 }
 
 // Storing G and Fx matrices in the jacob_motion package
 struct jacob_motion_package {
-
+    Eigen::MatrixXd Fx;
+    Eigen::MatrixXd G;
 };
+
+// Function to calculate jacobian motion
+jacob_motion_package jacob_motion(const Eigen::MatrixXd* x, const Eigen::MatrixXd& u, double dt) {
+
+
+    // Creating Identity Matrix of Size 3 x 3
+    Eigen::MatrixXd identityMatrix = Eigen::MatrixXd::Identity(STATE_SIZE, STATE_SIZE);
+
+
+    // Creating Zeroes Matrix
+    int n_lm = calc_n_lm(x);
+    Eigen::MatrixXd zerosMatrix(STATE_SIZE, LM_SIZE * n_lm);
+    zerosMatrix.setZero();
+
+    // Stacking Identity and Zeroes Matrix
+    Eigen::MatrixXd Fx(STATE_SIZE, STATE_SIZE + (LM_SIZE * n_lm));
+    Eigen::MatrixXd Fx << identityMatrix, zeroesMatrix;
+
+    Eigen::MatrixXd jF;
+
+    // Creating jF matrix
+    jF << 0.0, 0.0, -dt * u(0, 0) * std::sin(x(2, 0)),
+          0.0, 0.0, dt * u(0, 0) * std::cos(x(2, 0)),
+          0.0, 0.0, 0.0;
+
+    // Creating Fx transpose matrix
+    Eigen::MatrixXd FxT = Fx.transpose();
+
+    // Creating G matrix 
+    Eigen::MatrixXd G = identityMatrix + (FxT * jF * Fx);
+
+    // Constructing jacob motion package result
+    jacob_motion_package result;
+    result.Fx = Fx
+    result.G = G
+
+    return result
+}
+
+
+// Function to return (x, y) position of landmark stored as 2 x 1 matrix
+Eigen::MatrixXd calc_landmark_position(const Eigen::MatrixXd& x, const Eigen::MatrixXd& z) {
+
+    Eigen::MatrixXd zp(2, 1);
+    zp.setZero();
+
+    zp(0,0) = x(0, 0) + (z(0, 0) * std::cos(x(2, 0) + z(1, 0)));
+    zp(1,0) = x(1, 0) + (z(0, 0) * std::sin(x(2, 0) + z(1, 0)));
+
+    return zp;
+}
+
+Eigen::MatrixXd get_landmark_position_from_state(const Eigen::MatrixXd& x, int ind) {
+    
+    // Calculate the starting and ending row indices for the landmark position
+    int start_row = STATE_SIZE + LM_SIZE * ind;
+    int end_row = STATE_SIZE + LM_SIZE * (ind + 1);
+
+    // Extract the landmark position from the state vector
+    Eigen::MatrixXd lm = x.block(start_row, 0, LM_SIZE, 1);
+
+    return lm;
+}
+
+
+
+
+
 
 
 
