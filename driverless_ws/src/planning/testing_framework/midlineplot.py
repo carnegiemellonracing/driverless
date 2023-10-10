c49e77d354c2e0135e08c835b48da28e374ee51a
lakshmi-adiga <ladiga@andrew.cmu.edu>
2023-10-09 22:40:00 -0400

    Update midlineplot.py

--- driverless_ws/src/planning/testing_framework/midlineplot.py
+++ driverless_ws/src/planning/testing_framework/midlineplot.py
@@ -1,39 +1,44 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy import interpolate 
-from mpl_toolkits import mplot3d
+from scipy.interpolate import interp1d
 
 def plot_lin_input(blue_cone_pos, yellow_cone_pos, midline):
     bx = []
     by = []
-    bz = []
     
     yx = []
     yy = []
-    yz = []
     
-    ax = plt.axes(projection = '3d')
+    mx = []
+    my = []
     
     for i in range(len(blue_cone_pos)):
         bx.append(blue_cone_pos[i][0])
         by.append(blue_cone_pos[i][1])
-        bz.append(blue_cone_pos[i][2])
     
     for j in range(len(yellow_cone_pos)):
         yx.append(yellow_cone_pos[j][0])
         yy.append(yellow_cone_pos[j][1])
-        yz.append(blue_cone_pos[j][2])
+        
+    for k in range(len(midline)):
+        mx.append(midline[k][0])
+        my.append(midline[k][1])
+        
+    mx_interp = np.linspace(np.min(mx), np.max(mx), 100)
+    my_linear = interp1d(mx, my)
 
-    ax.plot3D(bx, by, bz, 'b*', yx, yy, yz, 'y*')
+
+    plt.plot(bx, by, 'b*', yx, yy, 'y*', mx, my, 'r*', mx_interp, my_linear(mx_interp), "black")
     plt.show()
 
 def plot_cub_input(blue_con_pos, yellow_cone_pos, midline):
     return 0;
 
 def test():
-    blue_cone_pos = [(0,0, 0), (1, 1, 1), (2, 2, 2), (3,3, 3)]
-    yellow_cone_pos = [(0, 1, 1), (1, 2, 2), (2, 3, 3), (3, 4, 4)]
-    plot_lin_input(blue_cone_pos, yellow_cone_pos, [0])
+    blue_cone_pos = [(0,0), (1, 1), (2, 2), (3,3)]
+    yellow_cone_pos = [(0, 1), (1, 2), (2, 3), (3, 4)]
+    midline = [(1, 0), (3, 1), (4, 2), (0, 3)]
+    plot_lin_input(blue_cone_pos, yellow_cone_pos, midline)
     
 test()
 
