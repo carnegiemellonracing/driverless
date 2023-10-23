4070476d51af8603eee4a70990b4b7bad6d0e84d
sfu699 <syfu@andrew.cmu.edu>
2023-10-23 16:47:53 -0400

    basic unit tests for a line of cones

--- driverless_ws/src/planning/src/midpoint_tests.cpp
+++ driverless_ws/src/planning/src/midpoint_tests.cpp
@@ -5,29 +5,95 @@
 int main(){
     //test case 1:
     printf("test case 1: \n");
-    gsl_matrix *left_1 = gsl_matrix_alloc(2,1);
-    gsl_matrix *right_1 = gsl_matrix_alloc(2,1);
-    gsl_matrix_set(left_1, 0, 0, 0);
-    gsl_matrix_set(left_1, 1, 0, 0);
-    gsl_matrix_set(right_1, 0, 0, 0);
-    gsl_matrix_set(right_1, 1, 0, 1);
-    gsl_matrix *result = midpoint(left_1, right_1);
+    gsl_matrix *left = gsl_matrix_alloc(2,1);
+    gsl_matrix *right = gsl_matrix_alloc(2,1);
+    gsl_matrix_set(left, 0, 0, 0);
+    gsl_matrix_set(left, 1, 0, 0);
+    gsl_matrix_set(right, 0, 0, 0);
+    gsl_matrix_set(right, 1, 0, 1);
+    gsl_matrix *result = midpoint(left, right);
     assert(gsl_matrix_get(result, 0, 0, 0));
     assert(gsl_matrix_get(result, 1, 0, 0.5));
+    free(left);
+    free(right);
+    free(result);
+
 
     //test case 2:
     printf("test case 2: \n");
-    gsl_matrix *left_1 = gsl_matrix_alloc(2,2);
-    gsl_matrix *right_1 = gsl_matrix_alloc(2,1);
-    gsl_matrix_set(left_1, 0, 0, 0);
-    gsl_matrix_set(left_1, 1, 0, 0);
-    gsl_matrix_set(left_1, 0, 1, 1);
-    gsl_matrix_set(left_1, 1, 1, 0);
+    gsl_matrix *left = gsl_matrix_alloc(2,2);
+    gsl_matrix *right = gsl_matrix_alloc(2,1);
+    gsl_matrix_set(left, 0, 0, 0);
+    gsl_matrix_set(left, 1, 0, 0);
+    gsl_matrix_set(left, 0, 1, 1);
+    gsl_matrix_set(left, 1, 1, 0);
 
-    gsl_matrix_set(right_1, 0, 0, 0);
-    gsl_matrix_set(right_1, 1, 0, 1);
-    gsl_matrix *result = midpoint(left_1, right_1);
+    gsl_matrix_set(right, 0, 0, 0);
+    gsl_matrix_set(right, 1, 0, 1);
+
+    gsl_matrix *result = midpoint(left, right);
+
+    assert(result->size2 == 2);
     assert(gsl_matrix_get(result, 0, 0, 0));
     assert(gsl_matrix_get(result, 1, 0, 0.5));
+    assert(gsl_matrix_get(result, 0, 1, 0.5));
+    assert(gsl_matrix_get(result, 1, 1, 0.5));
+    free(left);
+    free(right);
+    free(result);
+
+    //test case 3:
+    printf("test case 3: \n");
+    gsl_matrix *left = gsl_matrix_alloc(2,2);
+    gsl_matrix *right = gsl_matrix_alloc(2,2);
+    gsl_matrix_set(left, 0, 0, 0);
+    gsl_matrix_set(left, 1, 0, 0);
+    gsl_matrix_set(left, 0, 1, 1);
+    gsl_matrix_set(left, 1, 1, 0);
+
+    gsl_matrix_set(right, 0, 0, 0);
+    gsl_matrix_set(right, 1, 0, 1);
+    gsl_matrix_set(right, 0, 0, 1);
+    gsl_matrix_set(right, 1, 0, 1);
+
+    gsl_matrix *result = midpoint(left, right);
+
+    assert(result->size2 == 3);
+    assert(gsl_matrix_get(result, 0, 0, 0));
+    assert(gsl_matrix_get(result, 1, 0, 0.5));
+    assert(gsl_matrix_get(result, 0, 1, 0.5));
+    assert(gsl_matrix_get(result, 1, 1, 0.5));
+    assert(gsl_matrix_get(result, 0, 2, 1));
+    assert(gsl_matrix_get(result, 1, 2, 0.5));
+    free(left);
+    free(right);
+    free(result);
+
+    //test case 4:
+    printf("test case 4: \n");
+    gsl_matrix *left = gsl_matrix_alloc(2,2);
+    gsl_matrix *right = gsl_matrix_alloc(2,2);
+    gsl_matrix_set(left, 0, 0, 0);
+    gsl_matrix_set(left, 1, 0, 0);
+    gsl_matrix_set(left, 0, 1, 1);
+    gsl_matrix_set(left, 1, 1, 0);
+
+    gsl_matrix_set(right, 0, 0, 0);
+    gsl_matrix_set(right, 1, 0, 1);
+    gsl_matrix_set(right, 0, 0, 1);
+    gsl_matrix_set(right, 1, 0, 1);
+
+    gsl_matrix *result = midpoint(left, right);
+
+    assert(result->size2 == 3);
+    assert(gsl_matrix_get(result, 0, 0, 0));
+    assert(gsl_matrix_get(result, 1, 0, 0.5));
+    assert(gsl_matrix_get(result, 0, 1, 0.5));
+    assert(gsl_matrix_get(result, 1, 1, 0.5));
+    assert(gsl_matrix_get(result, 0, 2, 1));
+    assert(gsl_matrix_get(result, 1, 2, 0.5));
+    free(left);
+    free(right);
+    free(result);
 
 }
