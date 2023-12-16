ee1eead41ea39f978c2513c13444a730632f9f33
Griffin Teller <gteller@andrew.cmu.edu>
2023-12-15 20:50:15 -0500

    redid multithreading setup, created superclasses for cuda/cpu environments

--- driverless_ws/src/controls/src/mppi/mppi.hpp
+++ driverless_ws/src/controls/src/mppi/mppi.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
+#include <common/types.hpp>
+
 namespace controls {
     namespace mppi {
-        class mppi_controller {
+        class mppi_controller : public controller {
+        public:
+            mppi_controller () = default;  // not strictly necessary but emphasizes
+                                           // this is the intended way to construct
 
+            action generate_action(const state &current_state) override;
         };
     }
 }
