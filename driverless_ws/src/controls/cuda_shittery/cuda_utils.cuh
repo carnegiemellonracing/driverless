1388a93d50dadd0436cdcd7cfa46b279a2d79381
Griffin Teller <gteller@andrew.cmu.edu>
2024-01-16 21:43:46 -0500

    MASSIVE normal generation improvement

--- driverless_ws/src/controls/cuda_shittery/cuda_utils.cuh
+++ driverless_ws/src/controls/cuda_shittery/cuda_utils.cuh
@@ -1,15 +1,23 @@
 #pragma once
 
+#include <curand.h>
+
 #define IDX_2D_PITCHED(tensor, idx, pitch) ((decltype(tensor))((char*)tensor + idx.y * pitch + idx.x * sizeof(std::remove_pointer<decltype(tensor)>::type))
-#define IDX_3D_PITCHED(tensor, dims, idx, pitch) ((decltype(tensor))((char*)tensor + idx.z * dims.y * pitch + idx.y * pitch + idx.x * sizeof(std::remove_pointer<decltype(tensor)>::type)))
-#define IDX_3D(tensor, dims, idx) (&tensor[idx.z * dims.y * dims.x + idx.y * dims.x + idx.x])
+#define IDX_3D_PITCHED(tensor, dims, idx, pitch) ((decltype(tensor))((char*)tensor + idx.x * dims.y * pitch + idx.y * pitch + idx.z * sizeof(std::remove_pointer<decltype(tensor)>::type)))
+#define IDX_3D(tensor, dims, idx) (&tensor[idx.x * dims.y * dims.z + idx.y * dims.z + idx.z])
 
 #define CUDA_CALL(x) (cuda_assert(x, __FILE__, __LINE__))
 
 #define CURAND_CALL(x) (curand_assert(x, __FILE__, __LINE__))
 
+/**
+ * @brief 3D pitched tensor
+ */
+template<typename T>
 struct cudaTensor3D {
     cudaPitchedPtr pitched_ptr;
+
+    /** @brief Extent in bytes */
     cudaExtent extent;
 };
 
@@ -71,9 +79,37 @@ static void cuda_assert(cudaError_t code, const char *file, int line, bool abort
 
 static void curand_assert(curandStatus_t code, const char *file, int line, bool abort=true)
 {
-    if (code != cudaSuccess)
+    if (code != CURAND_STATUS_SUCCESS)
     {
         fprintf(stderr,"curand assert: %s. Location: %s:%d\n", curandGetErrorString(code), file, line);
         if (abort) exit(code);
+    }
+}
+
+template<typename T>
+static cudaError_t alloc_tensor3D_unpitched(cudaTensor3D<T>& tensor, dim3 dims) {
+    // yes, dims.z is correct (cuda uses column major here)
+    tensor.extent = make_cudaExtent(dims.z * sizeof(T), dims.y, dims.x);
+    tensor.pitched_ptr = make_cudaPitchedPtr(nullptr, tensor.extent.width, tensor.extent.width, tensor.extent.height);
+    return cudaMalloc(&tensor.pitched_ptr.ptr, tensor.extent.width * tensor.extent.height * tensor.extent.depth);
+}
+
+__device__ static uint3 total_idx(dim3 blockDim, uint3 blockIdx, uint3 threadIdx) {
+    return {
+        blockDim.x * blockIdx.x + threadIdx.x,
+        blockDim.y * blockIdx.y + threadIdx.y,
+        blockDim.z * blockIdx.z + threadIdx.z
+    };
+}
+
+__device__ static size_t flat_block_idx(dim3 blockDim, uint3 threadIdx) {
+    return threadIdx.x * blockDim.y * blockDim.z + threadIdx.y * blockDim.z + threadIdx.z;
+}
+
+template<typename T>
+__device__ static void thread_parallel_memcpy(T* dst, T* src, size_t elems, dim3 blockDim, uint3 threadIdx) {
+    const size_t offset = flat_block_idx(blockDim, threadIdx);
+    for (size_t i = offset; i < elems; i += blockDim.x * blockDim.y * blockDim.z) {
+        dst[i] = src[i];
     }
 }
