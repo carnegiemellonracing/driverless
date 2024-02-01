8596af7ff1c4009e440d8d2f763a8e076267bc00
ChipTheCincoze <geoffthejetson@gmail.com>
2024-02-01 17:47:58 -0500

    added launch files for predictors and sensors

--- driverless_ws/src/perceptions/setup.py
+++ driverless_ws/src/perceptions/setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 from setuptools import find_namespace_packages
 
+from glob import glob
+import os
+
 package_name = 'perceptions'
 
 setup(
@@ -11,6 +14,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
