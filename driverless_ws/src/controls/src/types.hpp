3ed4053b578221cb6add4492cd6eb5b6945af17f
Griffin Teller <gteller@andrew.cmu.edu>
2024-02-03 16:22:27 -0500

    began integration. entirely broken

--- driverless_ws/src/controls/src/types.hpp
+++ driverless_ws/src/controls/src/types.hpp
@@ -1,21 +1,32 @@
 #pragma once
 
-#include <interfaces/msg/spline_list.hpp>
+#include <state/state_estimator.hpp>
 #include <array>
 
 #include "constants.hpp"
 
 
 namespace controls {
-    using Action = std::array<double, action_dims>;
-    using State = std::array<double, state_dims>;
+    using Action = std::array<float, action_dims>;
+    using State = std::array<float, state_dims>;
 
-    using SplineMsg = interfaces::msg::SplineList;
+    union SplineFrame {
+        float4 texel;
+
+        struct {
+            float x;
+            float y;
+            float tangent_angle;
+            float curvature;
+        };
+    };
+
+    using SplineMsg = std::vector<SplineFrame>;
     using SlamMsg = struct {};
 
     class Controller {
     public:
-        virtual Action generate_action(const State &current_state) =0;
+        virtual Action generate_action() =0;
 
         virtual ~Controller() =0;
     };
