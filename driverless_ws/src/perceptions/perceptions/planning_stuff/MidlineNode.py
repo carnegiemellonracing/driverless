a1d2c5ebba2050dc58d353a052aa59f85534192b
dale <geoffthejetson@gmail.com>
2024-02-10 12:19:06 -0500

    svm midline

---
+++ driverless_ws/src/perceptions/perceptions/planning_stuff/MidlineNode.py
@@ -0,0 +1,63 @@
+
+import rclpy
+from rclpy.node import Node
+from eufs_msgs.msg import ConeArray
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy, QoSDurabilityPolicy
+import perceptions.planning_stuff.svm_utils as svm_utils
+
+import numpy as np
+
+
+# configure QOS profile
+BEST_EFFORT_QOS_PROFILE = QoSProfile(reliability = QoSReliabilityPolicy.BEST_EFFORT,
+                         history = QoSHistoryPolicy.KEEP_LAST,
+                         durability = QoSDurabilityPolicy.VOLATILE,
+                         depth = 5)
+
+RELIABLE_QOS_PROFILE = QoSProfile(
+    depth=10,
+    reliability=QoSReliabilityPolicy.RELIABLE,
+    durability=QoSDurabilityPolicy.VOLATILE,
+    history=QoSHistoryPolicy.KEEP_LAST,
+)
+
+
+
+class MidlineNode(Node):
+
+    def __init__(self):
+        super().__init__("midline_node")
+        self.cone_sub = self.create_subscription(msg_type=ConeArray,
+                                                 topic="/perc_cones",
+                                                 callback=self.cone_callback,
+                                                 qos_profile=BEST_EFFORT_QOS_PROFILE)
+        # self.midline_pub = self.create_publisher(msg_type=)
+    
+    def cone_callback(cones):
+        blue = np.array()
+        for cone in cones.blue_cones:
+            np.append(blue, np.array([cone.x, cone.y, 0]))
+
+        yellow = np.array()
+        for cone in cones.yellow_cones:
+            np.append(yellow, np.array([cone.x, cone.y, 1]))
+        
+        data = np.vstack([blue, yellow])
+        print(data)
+
+        downsampled_boundary_points = svm_utils.process(data)
+
+        #TODO: convert np array to rosmsg
+
+
+def main():
+    print("hello world")
+    rclpy.init()
+    midline_node = MidlineNode()
+    rclpy.spin(midline_node)
+
+    midline_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ driverless_ws/src/perceptions/perceptions/planning_stuff/svm_utils.py
@@ -0,0 +1,46 @@
+
+from sklearn import svm
+import numpy as np
+
+def process(data):
+
+    X = data[:, :2]
+    y = data[:, -1]
+    model = svm.SVC(kernel='poly', degree=3, C=10, coef0=1.0)
+    model.fit(X, y)
+
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    print(x_min, x_max)
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01),
+                        np.arange(y_min, y_max, 0.01))
+    
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    boundary_points = []
+    for i in range(1, len(xx)):
+        for j in range(1, len(xx[0])):
+            if Z[i][j] != Z[i-1][j-1]:
+                boundary_points.append([xx[i][j], yy[i][j]])
+
+    def norm_func(x): return np.sqrt(x[0]**4 + x[1]**2)
+    boundary_points.sort(key=norm_func)
+
+    downsampled = []
+    accumulated_dist = 0
+    for i in range(1, len(boundary_points)):
+        p1 = boundary_points[i]
+        p0 = boundary_points[i-1]
+        curr_dist = np.sqrt((p1[0] - p0[0])**2 + (p1[1] - p0[1])**2)
+        accumulated_dist += curr_dist
+        if np.abs(accumulated_dist - 0.5) < 0.05:
+            downsampled.append(p1)
+            accumulated_dist = 0
+        
+        if accumulated_dist > 0.55:
+            accumulated_dist = 0
+    
+    downsampled = np.array(list(downsampled))
+
+    return downsampled
