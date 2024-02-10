c136fdd12ecdb94d65c73a34066b861206615b45
Ayush548 <ayushgarg0728@gmail.com>
2024-02-10 18:46:07 -0500

    Update unit_tests.cpp

--- driverless_ws/src/controls/tests/unit_tests.cpp
+++ driverless_ws/src/controls/tests/unit_tests.cpp
@@ -3,7 +3,7 @@
 
 namespace model {
 
-    bool equals(float[] arr1, float[]arr2, int length)
+    bool arrequals(float[] arr1, float[]arr2, int length)
     @requires sizeof(arr1) == length;
     @requires sizeof(arr2) == length;
     {
@@ -16,33 +16,57 @@ namespace model {
         return true;
     }
 
-
-    // The fixture for testing class Foo.
-    class ModelTest : public testing::Test {
-    protected:
-
-        ModelTest() {
-            // You can do set-up work for each test here.
+    void copy(float[] arr1, float[]arr2, int length)
+    @requires sizeof(arr1) == length;
+    @requires sizeof(arr2) == length;
+    {
+        for (int i=0; i<length; i++) {
+             arr2[i] = arr1[i];
         }
+    }
 
-        float state[10] = [0,0,0,0,0,0,0,0,0,0];
-        float state_dot[10] = [0,0,0,0,0,0,0,0,0,0];
-        float action[10] = [0,0,0];
 
-    };
-
-TEST_F(ModelTest, sanityCheck) {
+TEST(ModelTest, NoDependenceOnWorldCords) {
+    //rand stuff
+    float state[10] = [0,0,54,3,3,-3,42,-43,2,89];
+    float state_dot[10] = [0,0,0,0,0,0,0,0,0,0];
+    float action[10] = [-2,12,-65];
    //Tests that nothing matters based on X_World and Y_world
-   dynamics(state, action, state_dot);
-
+    dynamics(state, action, state_dot);
+    float state_dot_2 = copy(state_dot,state_dot_2, 10);
+    state[0] = 12;
+    state[1] = -12;
+    dynamics(state, action, state_dot);
+    EXPECT_EQ(equals(state_dot_2,state_dot), true)
 }
+
+
+TEST(ModelTest, Nothing_Gives_Nothing) {
+    //rand stuff
+    float state[10] = [0,0,0,0,0,0,0,0,0,0];
+    float state_dot_ex[10] = [0,0,0,0,0,0,0,0,0,0];
+    float state_dot_res[10] = [0,0,0,0,0,0,0,0,0,0];
+    float action[10] = [0,0,0];
+    dynamics(state, action, state_dot_res);
+    EXPECT_EQ(equals(state_dot_ex,state_dot_res), true)
+}
+
+
+TEST(ModelTest, Nothing_Gives_Nothing) {
+    //rand stuff
+    float state[10] = [0,0,0,0,0,0,0,0,0,0];
+    float state_dot_ex[10] = [0,0,0,0,0,0,0,0,0,0];
+    float state_dot_res[10] = [0,0,0,0,0,0,0,0,0,0];
+    float action[10] = [0,0,0];
+    dynamics(state, action, state_dot_res);
+    EXPECT_EQ(equals(state_dot_ex,state_dot_res), true)
+}
+
+
 
 } //model namespace
 
 
-TEST(controls_lib, stub_test) {
-ASSERT_EQ(1, 1);
-}
 
 int main(int argc, char* argv[]) {
     testing::InitGoogleTest(&argc, argv);
