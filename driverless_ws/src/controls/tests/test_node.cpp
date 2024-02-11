d3d564b9964bbc5ef6fe449e97caf9ebeea05a32
Griffin Teller <gteller@andrew.cmu.edu>
2024-02-10 19:08:34 -0500

    IT RUNS! produces nans tho

---
+++ driverless_ws/src/controls/tests/test_node.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <types.hpp>
+#include <constants.hpp>
+#include <interfaces/msg/control_action.hpp>
+#include <interfaces/msg/spline_frame_list.hpp>
+#include <interfaces/msg/spline_list.hpp>
+#include <cmath>
+#include <rclcpp/rclcpp.hpp>
+#include <glm/glm.hpp>
+#include <std_msgs/msg/header.hpp>
+
+#include "test_node.hpp"
+
+
+namespace controls {
+    namespace tests {
+        TestNode::TestNode()
+            : Node{"test_node"},
+
+              m_subscriber (create_subscription<interfaces::msg::ControlAction>(
+                    control_action_topic_name, control_action_qos,
+                    [this] (const interfaces::msg::ControlAction::SharedPtr msg) { print_message(*msg); })),
+
+              m_spline_publisher {create_publisher<interfaces::msg::SplineFrameList>("spline", spline_qos)},
+
+              m_spline_timer {create_wall_timer(
+                  std::chrono::duration<float, std::milli>(1000),
+                    [this]{ publish_spline(); })} {
+
+        }
+
+        interfaces::msg::SplineFrameList
+        sine_spline(float period, float amplitude, float progress, float density) {
+            using namespace glm;
+
+            interfaces::msg::SplineFrameList result {};
+
+            fvec2 point {0.0f, 0.0f};
+            float total_dist = 0;
+
+            while (total_dist < progress) {
+                interfaces::msg::SplineFrame frame {};
+                frame.x = point.x;
+                frame.y = point.y;
+                result.frames.push_back(std::move(frame));
+
+                fvec2 delta = normalize(fvec2(1.0f, amplitude * 2 * M_PI / period * cos(2 * M_PI / period * point.x)))
+                            * density;
+                total_dist += length(delta);
+                point += delta;
+            }
+
+            result.header = std_msgs::msg::Header {};
+            return result;
+        }
+
+        void TestNode::print_message(const interfaces::msg::ControlAction& msg) {
+            // std::cout << "Swangle: " << msg.swangle << " Torque f: " <<
+            //     msg.torque_f << " Torque r: " << msg.torque_r << std::endl;
+        }
+
+        void TestNode::publish_spline() {
+            std::cout << "Publishing spline" << std::endl;
+            const auto spline = sine_spline(10, 2.5, 100, 0.5);
+            m_spline_publisher->publish(spline);
+        }
+    }
+}
+
+int main(int argc, char* argv[]){
+    rclcpp::init(argc, argv);
+
+    rclcpp::spin(std::make_shared<controls::tests::TestNode>());
+
+    rclcpp::shutdown();
+    return 0;
+}
--- driverless_ws/src/controls/tests/unit_tests.cpp
+++ driverless_ws/src/controls/tests/unit_tests.cpp
@@ -1,10 +1,44 @@
 #include <gtest/gtest.h>
+#include <mppi/functors.cuh>
 
-TEST(controls_lib, stub_test) {
-    ASSERT_EQ(1, 1);
+namespace controls {
+    namespace mppi {
+        class MPPITest : public testing::Test {
+        protected:
+            void SetUp() override {
+                device_action1 {{1.0F, 2.0F, 3.0F}};
+                device_action2 {{0.2F, 3.0F, 1.5F}};
+                device_action3 {{-0.6F, 1.4F, 1.0F}};
+            }
+
+            DeviceAction device_action1;
+            DeviceAction device_action2;
+            DeviceAction device_action3;
+
+        };
+    }
+
+
+    TEST(controls_lib, stub_test) {
+        ASSERT_EQ(1, 1);
+
+    }
+
+
+
+class ModelTest : public testing::Test {
+protected:
+
+    ModelTest() {
+        // You can do set-up work for each test here.
+    }
+
+    // Class members declared here can be used by all tests in the test suie
+};
+
+    TEST_F(ModelTest, sanityChecks){
+
 }
 
-int main(int argc, char* argv[]) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+
+
