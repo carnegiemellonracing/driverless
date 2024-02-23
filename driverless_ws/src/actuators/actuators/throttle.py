f195e2bf8e638062285394d3c9efbbf795686cb2
ChipTheCincoze <geoffthejetson@gmail.com>
2024-02-23 14:34:57 -0500

    throttle.py works

--- driverless_ws/src/actuators/actuators/throttle.py
+++ driverless_ws/src/actuators/actuators/throttle.py
@@ -1,39 +1,51 @@
 import rclpy
 from rclpy.node import Node
-# from interfaces.msg import ControlAction
+from interfaces.msg import ControlAction
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSDurabilityPolicy
 from std_msgs.msg import Int32
 import can
 import time
+import math
 
 BUSTYPE = 'pcan'
 CHANNEL = 'PCAN_USBBUS1'
 BITRATE = 500000
 TIMER_HZ = 100
 MAX_TORQUE = 2000 #this is completely made up
-MAX_REQUEST = 200 #hypothetically real max is 255
+MAX_REQUEST = 255 #hypothetically real max is 255
+
+ADC_BIAS = 2212
+SLOPE = 34.5
+
+CMDLINE_QOS_PROFILE = QoSProfile(
+    depth=10,  # Set the queue depth
+    reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_RELIABLE,  # Set the reliability policy
+    durability=QoSDurabilityPolicy.RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL  # Set the durability policy
+)
 
 class ActuatorNode(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
         self.bus2 = can.interface.Bus(bustype=BUSTYPE, channel=CHANNEL, bitrate=BITRATE)
-        # self.subscription = self.create_subscription(
-        #     ControlAction,
-        #     'control_action',  # Replace with the desired topic name
-        #     self.callback,
-        #     10  # Adjust the queue size as needed
-        # )5
-        self.int_subscription = self.create_subscription(
-           Int32,
-           '/swangle',
-           self.swangle_callback,
-           10
+        self.subscription = self.create_subscription(
+            ControlAction,
+            '/control_action',  # Replace with the desired topic name
+            self.callback,
+            CMDLINE_QOS_PROFILE
         )
+        # self.int_subscription = self.create_subscription(
+        #    Int32,
+        #    '/swangle',
+        #    self.swangle_callback,
+        #    10
+        # )
         timer_period = 1/TIMER_HZ  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.even = True
-        self.swangle = int(hex(2280)[2:], 16)
-        self.accumulator = 0
+        # self.even = True
+        self.swangle = int(hex(ADC_BIAS)[2:], 16)
+        self.torque_request = 0
+        # self.accumulator = 0
 
     def timer_callback(self):
         # to test regen: send full throttle, then full negative for regen
@@ -46,28 +58,40 @@ class ActuatorNode(Node):
         # steering_bytearr = swangle.to_bytes()
 
         # first element in data array is throttle(uint8), next 2 is steering (int16)
-        msg = can.Message(arbitration_id=0x134, data = [0x00ff & self.swangle, (0xff00 & self.swangle)>>8, 6,5,7,8,9,1], is_extended_id=False)
-        self.bus2.send(msg)
-        if (self.accumulator % 50 == 0):
-            print(f"Message Sent on {self.bus2.channel_info}, data: {msg.data}")
-        self.accumulator += 1
-        # self.even = not self.even
+        #where is this arbitration_id coming from? is this just our CAN ID?
+        throttle_msg = can.Message(arbitration_id=0x2D2, data = [self.torque_request]*8, is_extended_id=False)
+        self.bus2.send(throttle_msg)
+        print(f"Throttle Message Sent: {throttle_msg.data}")
+
+        steering_msg = can.Message(arbitration_id=0x134, data = [0x00ff & self.swangle, (0xff00 & self.swangle)>>8, 6,5,7,8,9,1], is_extended_id=False)
+        self.bus2.send(steering_msg)
+        print(f"steering Message Sent: {steering_msg.data}")
+        print("----------------------")
     
     def callback(self,msg):
+        #TODO: SHIFT TORQUE VALUE TO BE CENTERED AROUND 128
         (fl, fr, rl, rr) = (msg.torque_fl, msg.torque_fr, msg.torque_rl, msg.torque_rr)
+        print(f"fl: {fl} |fr: {fr} | rl: {rl} | rr: {rr}")
         torque_avg = (fl+fr+rl+rr)/4
         torque_percent = min(1,max((torque_avg/MAX_TORQUE),-1)) #fucked clamping fix later
 
         #TODO: Read through CDC code and figure out what max regen request is to clamp in the negative direction
 
-        torque_request = int(torque_percent*MAX_REQUEST)
+        self.torque_request = int(torque_percent*127 + 128)
+        print(self.torque_request) 
 
-        #where is this arbitration_id coming from? is this just our CAN ID?
-        can_msg = can.Message(arbitration_id=0x134, data = [torque_request]*8, is_extended_id=False)
-        self.bus2.send(can_msg)
+        desired_wheel_angle = msg.swangle * 180/math.pi # convert from radians to degrees
+        clamped_wheel_angle = min(max(desired_wheel_angle, -20), 20)
+        adc_val = int(SLOPE * clamped_wheel_angle + ADC_BIAS)
+        self.swangle = int(hex(adc_val)[2:], 16)
+        print(f"swangle: {self.swangle}")
+
+        
     
     def swangle_callback(self,msg):
-        self.swangle = int(hex(msg.data)[2:], 16)
+        desired_wheel_angle = msg.data
+        adc_val = int(34.5 * desired_wheel_angle + 2212)
+        self.swangle = int(hex(adc_val)[2:], 16)
     
 def main(args=None):
     rclpy.init(args=args)
