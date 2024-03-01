7fcca8e7c60e68e541ccef9cc09d19d48885150c
ChipTheCincoze <geoffthejetson@gmail.com>
2024-03-01 14:01:45 -0500

    iFixed filenode

--- driverless_ws/src/perceptions/perceptions/ros/utils/FileNode.py
+++ driverless_ws/src/perceptions/perceptions/ros/utils/FileNode.py
@@ -14,7 +14,7 @@ import numpy as np
 
 # set to determine what folder to create (find in ~/driverless/driverless_ws/<FOLDER_NAME>)
 # DO NOT MAKE "src", "build", "install", or "log"
-FOLDER_NAME = "three-laps-0-subset"
+FOLDER_NAME = "three-laps-large"
 
 # define path to data directory
 WS_DIR = Path(__file__).parents[3]
