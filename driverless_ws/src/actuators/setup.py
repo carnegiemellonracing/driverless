f29f13cba94205ef47d5b04211e902093f7ccc95
ChipTheCincoze <geoffthejetson@gmail.com>
2024-03-21 14:36:55 -0400

    modified actuator node

--- driverless_ws/src/actuators/setup.py
+++ driverless_ws/src/actuators/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'throttle_node = actuators.throttle:main',
+            'throttle_node = actuators.throttleUSB:main',
         ],
     },
 )
