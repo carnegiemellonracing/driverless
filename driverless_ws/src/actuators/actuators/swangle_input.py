295fcd696c2d7cd996aac8b515ce538260be1063
ChipTheCincoze <geoffthejetson@gmail.com>
2024-03-26 16:53:53 -0400

    added controls build script

--- driverless_ws/src/actuators/actuators/swangle_input.py
+++ driverless_ws/src/actuators/actuators/swangle_input.py
@@ -1,40 +1,51 @@
-import throttleUSB
 import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSDurabilityPolicy
 from interfaces.msg import ControlAction
 import numpy as np
+import threading
 
 
-class SwangleInputNode(rclpy.Node):
-    def __init(self):
+CMDLINE_QOS_PROFILE = QoSProfile(
+    depth=1,  # Set the queue depth
+    reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_RELIABLE,  # Set the reliability policy
+    durability=QoSDurabilityPolicy.RMW_QOS_POLICY_DURABILITY_VOLATILE  # Set the durability policy
+)
+
+class SwangleInputNode(Node):
+    def __init__(self):
         super().__init__('swangle_input')
 
         self.publisher = self.create_publisher(
             ControlAction,
             '/control_action',  # Replace with the desired topic name
-            throttleUSB.CMDLINE_QOS_PROFILE
+            CMDLINE_QOS_PROFILE
         )
 
     def publish(self, swangle):
+        swangle = float(swangle)
         action = ControlAction()
         action.swangle = np.radians(swangle)
-        action.torque_fl = -100
-        action.torque_fr = -100
-        action.torque_rl = -100
-        action.torque_rr = -100
+        action.torque_fl = -100.0
+        action.torque_fr = -100.0
+        action.torque_rl = -100.0
+        action.torque_rr = -100.0
         self.publisher.publish(action)
 
+def main(args=None):
+    rclpy.init(args=args)
 
-def main():
     publisher = SwangleInputNode()
-    rclpy.spin(publisher)
+    spinner = threading.Thread(target=rclpy.spin, args=(publisher,))
+    spinner.start()
 
     try:
         while True:
             swangle = input("Swangle (deg): ")
             publisher.publish(swangle)
 
-    except BaseException:
-        pass
+    except BaseException as e:
+        print(e)
 
     publisher.destroy_node()
     rclpy.shutdown()
