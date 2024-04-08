08fcaeeb3a346eef2a03a68fa0aa4ef9ef2e1a58
Deep Patel <dmpatel@andrew.cmu.edu>
2024-04-08 12:28:30 -0400

    bug fix

--- driverless_ws/src/perceptions/setup.py
+++ driverless_ws/src/perceptions/setup.py
@@ -64,7 +64,7 @@ setup(
             'cone_node_any_debug = perceptions.ros.utils.ConeNode:main_any_debug',
 
             # simulated cones
-            'cone_sim_node = perceptions.ros.sim.ConeSimNoe:main',
+            'cone_sim_node = perceptions.ros.sim.ConeSimNode:main',
             'cone_sim_node_debug = perceptions.ros.sim.ConeSimNode:main_debug',
 
             # midline node
