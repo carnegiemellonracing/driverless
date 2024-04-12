ff4457dfd3844eeca8f21028ab0d828b5e973bef
dale <geoffthejetson@gmail.com>
2024-04-12 16:03:09 -0400

    websocketer drafted

---
+++ driverless_ws/src/websocketer/websocketer/WebsocketNode.py
@@ -0,0 +1,60 @@
+import rclpy
+from rclpy.node import Node
+from interfaces.msg import ConeArray, SplineFrames
+import message_filters
+import socket
+import string
+import os
+
+class WebsocketNode(Node):
+    def __init__(self, clientAddress, clientSocket):
+        super().__init__('websocketer_node')
+        self.cone_topic = "/perc_cones"
+        self.midline_topic = "/spline"
+
+        self.cone_subscribe = None
+        self.midline_subscribe = None
+
+        self.csocket = clientSocket
+        print("Connected to", clientAddress)
+        self.subscribe_topics()
+        
+    def subscribe_topics(self):
+        self.cone_subscribe = message_filters.Subscriber(self, ConeArray, self.cone_topic)
+        self.midline_subscribe = message_filters.Subscriber(self, SplineFrames, self.midline_topic)
+        self.ts = message_filters.ApproximateTimeSynchronizer([self.cone_subscribe, self.midline_subscribe], 10, 0.1)
+        self.ts.registerCallback(self.midline_upload_callback)
+        self.ts.registerCallback(self.cone_upload_callback)
+
+    def midline_upload_callback(self, midline_msg):
+        try:
+            self.csocket.send(midline_msg)
+            callback_confirmation = f"Message at {midline_msg.orig_data_stamp} Uploaded!"
+        except socket.error:
+            print("Error!")
+            return 
+
+    def cone_susbcription_callback(self, cone_msg):
+        try:
+            self.csocket.send(cone_msg)
+            callback_confirmation = f"Message at {cone_msg.orig_data_stamp} Uploaded!"
+        except socket.error:
+            print("Error!")
+            return 
+
+
+def main(args=None):
+    LOCALHOST = ''
+    PORT = 8080
+
+    rclpy.init(args=args)
+
+    while True:
+        clientAddress, clientSocket = server.accept()
+        websocketer = WebsocketNode(clientAddress, clientSocket)
+        rclpy.spin(websocket_node)
+        websocket_node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
