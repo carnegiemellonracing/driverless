a5ea4046f66c4362a26d3fbae37dd3e53cbe3179
ChipTheCincoze <geoffthejetson@gmail.com>
2024-04-19 13:21:11 -0400

    tt-4-18 code

--- driverless_ws/src/controls/src/constants.hpp
+++ driverless_ws/src/controls/src/constants.hpp
@@ -91,12 +91,12 @@ namespace controls {
     constexpr float gear_ratio = 15.0f;
     constexpr float car_mass = 210.0f;
     constexpr float rolling_drag = 100.0f; // N
-    constexpr float long_tractive_capability = 3.5f; // m/s^2
+    constexpr float long_tractive_capability = 2.0f; // m/s^2
     constexpr float lat_tractive_capability = 5.0f; // m/s^2
     constexpr float understeer_slope = 0.05f;
     constexpr float brake_enable_speed = 1.0f;
     constexpr float saturating_motor_torque = (long_tractive_capability + rolling_drag / car_mass) * car_mass * whl_radius / gear_ratio;
-    constexpr float approx_propogation_delay = 0.0f;  // sec
+    constexpr float approx_propogation_delay = 0.2f;  // sec
     constexpr float approx_mppi_time = 0.1f; // sec
 
     enum class TorqueMode
--- driverless_ws/src/perceptions/perceptions/ros/utils/FileNode.py
+++ driverless_ws/src/perceptions/perceptions/ros/utils/FileNode.py
@@ -17,7 +17,7 @@ import numpy as np
 
 # set to determine what folder to create (find in ~/driverless/driverless_ws/<FOLDER_NAME>)
 # DO NOT MAKE "src", "build", "install", or "log"
-FOLDER_NAME = "tt-4-6-lidar"
+FOLDER_NAME = "tt-4-18-eleventh"
 
 # define path to data directory
 WS_DIR = Path(__file__).parents[3]
