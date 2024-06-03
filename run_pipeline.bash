0b4e3fb9351dbe7b16b2f748cc1fa63b1c10062a
Anthony Yip <anthony.ch.yip@gmail.com>
2024-06-03 10:35:23 +0800

    Documented constants.hpp

--- run_pipeline.bash
+++ run_pipeline.bash
@@ -1,37 +1,33 @@
 tmux new-session -d;  # start new detached tmux session
 
 #rosbag
-tmux send 'source $DRIVERLESS/driverless_ws/install/setup.bash && clear' ENTER;
+tmux send 'source $DRIVERLESS/driverless_ws/install/setup.bash' ENTER;
 #tmux send 'ros2 bag record -a -o';   
 
 #perceptions
-tmux split-window -h -p 100;                            
-tmux send 'source $DRIVERLESS/driverless_ws/install/setup.bash && ros2 run perceptions lidar_node' ENTER;                    
+tmux split-window -h -p 100;  
+tmux send 'source ~/movella_ws/install/setup.bash && ros2 run bluespace_ai_xsens_mti_driver xsens_mti_node' ENTER;                    
 tmux split-window -v -p 70;
-tmux send 'source $DRIVERLESS/driverless_ws/install/setup.bash && ros2 run perceptions cone_node_lidar' ENTER;                    
-# tmux split-window -v -p 70;
-# tmux send 'source $DRIVERLESS/driverless_ws/install/setup.bash && ros2 run perceptions yolov5_zed_node' ENTER;                    
-# tmux split-window -h -p 100;                            
-# tmux send 'source $DRIVERLESS/driverless_ws/install/setup.bash && ros2 run perceptions yolov5_zed2_node' ENTER;                    
+tmux send 'source $DRIVERLESS/driverless_ws/install/setup.bash && ros2 run hesai_ros_driver hesai_ros_driver_node' ENTER;
 
 #planning
-tmux split-window -h -p 50;                             # split the detached tmux session
-tmux send 'source $DRIVERLESS/driverless_ws/install/setup.bash && ros2 run perceptions midline_node' ENTER;                    
-#TODO: raceline
-#TODO: slam
-
-#controls
-tmux split-window -h -p 50;                             # split the detached tmux session
-tmux send 'source $DRIVERLESS/driverless_ws/install/setup.bash && ros2 run controls controller' ENTER;    
-
-#actuators
+tmux select-pane -t 0
 tmux split-window -h -p 50;                             # split the detached tmux session
 tmux send 'source $DRIVERLESS/driverless_ws/install/setup.bash && ros2 run actuators throttle_node' ENTER;    
+tmux split-window -v -p 70;
+tmux send 'sudo mount /dev/sda /storage1' ENTER;         
+tmux send 'chip22a' ENTER;         
+tmux send 'cd /storage1' ENTER;         
 
-# #sbg
-tmux split-window -h -p 60;                             # split the detached tmux session
-tmux send 'source ~/movella_ws/install/setup.bash && ros2 launch bluespace_ai_xsens_mti_driver xsens_mti_node.launch.py';                    
+
+
+tmux select-pane -t 2
+tmux split-window -h -p 100;
+tmux send 'source $DRIVERLESS/driverless_ws/install/setup.bash && ros2 run perceptions endtoend_node' ENTER;                
+
+tmux select-pane -t 4
+tmux split-window -h -p 100;
+tmux send 'source $DRIVERLESS/driverless_ws/install/setup.bash && ros2 run controls controller' ENTER;
 
 # open (attach) tmux session.
 tmux a;                                                 
-
