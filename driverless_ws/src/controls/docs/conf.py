60b76a33344ace7d70164efece8ffd83d5feb43a
anthony-yip <anthony.ch.yip@gmail.com>
2024-06-14 18:27:30 +0800

    Base rst documentation using Breathe

---
+++ driverless_ws/src/controls/docs/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'driverless_controls'
+copyright = '2024, Anthony Yip'
+author = 'Anthony Yip'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+import os, sys
+sys.path.append(os.fspath('../venv/lib/python3.10/site-packages'))
+extensions = ['breathe']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+breathe_projects = {"driverless_controls": "doxyxml/"}
+breathe_default_project = "driverless_controls"
+breathe_default_members = ('members', 'protected-members', 'private-members', 'undoc-members')
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ driverless_ws/src/controls/docs/index.rst
@@ -0,0 +1,21 @@
+.. driverless_controls documentation master file, created by
+   sphinx-quickstart on Wed Jun 12 11:18:07 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to CMR's DV controls documentation!
+===============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   rst/explainers/terminology
+   rst/reference/api_reference
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
