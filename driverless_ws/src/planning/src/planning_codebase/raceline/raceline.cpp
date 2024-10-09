a6862dae392d6aa2f6bfc36544ee52892b64be82
vichan7 <victorih@andrew.cmu.edu>
2024-10-09 17:02:36 -0400

    part 2238523

--- driverless_ws/src/planning/src/planning_codebase/raceline/raceline.cpp
+++ driverless_ws/src/planning/src/planning_codebase/raceline/raceline.cpp
@@ -513,7 +513,8 @@ std::pair<std::vector<Spline>,std::vector<double>> raceline_gen(rclcpp::Logger l
 
         std::cout << group.rows() << group.cols() << std::endl;
         // shave off overlapping points from the spline if last group for og matrix and rotated matrix
-        group = group.block(0, flag, 2, group.cols());
+        // group = group.block(0, flag, 2, group.cols());
+        group = group.block(0, 1, 2, group.cols()-1);
         std::cout << "Checkpoint 2.5" << std::endl;
         rotated_points = rotated_points.block(0, flag, 2, group.cols());
 
