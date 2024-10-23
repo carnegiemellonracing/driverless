631d67ce9a1675e9168ab124d3a12e018fccfb1f
vichan7 <victorih@andrew.cmu.edu>
2024-10-23 17:25:49 -0400

    again

--- driverless_ws/src/planning/src/planning_codebase/raceline/chunkTest.cpp
+++ driverless_ws/src/planning/src/planning_codebase/raceline/chunkTest.cpp
@@ -154,12 +154,12 @@ int main() {
 
     std::vector<Chunk*> chunks = *(generateChunks(blue_cones, yellow_cones));
 
-    std::pair<double, double> xyBlue = chunks[0]->bluePoints[0].first;
+    double xBlue = chunks[0]->bluePoints[0].first;
     // std::pair<double, double> xyYellow = chunks[0]->yellowPoints
     
     std::cout << "CHUNK" << std::endl;
     std::cout << chunks[0]->avgCurvature << std::endl;
-    std::cout << xyBlue << std::endl;
+    std::cout << xBlue << std::endl;
     
     return 0;
 }
