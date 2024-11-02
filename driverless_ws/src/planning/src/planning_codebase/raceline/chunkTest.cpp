7ca6984c13c687ed2f303d4c30b605ba1f6a4ceb
vichan7 <victorih@andrew.cmu.edu>
2024-11-02 14:00:10 -0400

    test along

--- driverless_ws/src/planning/src/planning_codebase/raceline/chunkTest.cpp
+++ driverless_ws/src/planning/src/planning_codebase/raceline/chunkTest.cpp
@@ -15,88 +15,102 @@ double ftom(int a){
     return (double) a * 0.3048;
 }
 
+// std::vector<std::pair<double,double>> yellow_cones = {
+//     std::make_pair(0, 0),
+//     std::make_pair(0, 2),
+//     std::make_pair(0, 4),
+//     std::make_pair(0, 6),
+//     std::make_pair(0, 8),
+//     std::make_pair(0, 10),
+//     std::make_pair(0, 12),
+//     std::make_pair(0, 14),
+//     std::make_pair(0, 16),
+//     std::make_pair(0, 18),
+//     std::make_pair(0, 20),
+//     std::make_pair(0, 22),
+//     std::make_pair(0 - 6 + 6 * cos(ator(30)), 22 + 6 * sin(ator(30))),
+//     std::make_pair(0 - 6 + 6 * cos(ator(60)), 22 + 6 * sin(ator(60))),
+//     std::make_pair(-6, 28),
+//     std::make_pair(-8, 28),
+//     std::make_pair(-10, 28),
+//     std::make_pair(-12, 28),
+//     std::make_pair(-14, 28),
+//     std::make_pair(-14 + 6 * cos(ator(120)), 28 - 6 + 6 * sin(ator(120))),
+//     std::make_pair(-14 + 6 * cos(ator(150)), 28 - 6 + 6 * sin(ator(150))),
+//     std::make_pair(-20, 22),
+//     std::make_pair(-20 + ftom(4), 20),
+//     std::make_pair(-20 + ftom(6), 18),
+//     std::make_pair(-20 + ftom(2), 16),
+//     std::make_pair(-20 - ftom(2), 14),
+//     std::make_pair(-20 - ftom(6), 12),
+//     std::make_pair(-20 - ftom(4), 10),
+//     std::make_pair(-20, 8),
+//     std::make_pair(-20, 6),
+//     std::make_pair(-20, 4),
+//     std::make_pair(-16+2 - 6*cos(ator(30)),4-6*sin(ator(30))),
+//     std::make_pair(-16+2 - 6*cos(ator(60)),4-6*sin(ator(90))),
+//     std::make_pair(-14,-2),
+//     std::make_pair(-12,-2),
+//     std::make_pair(-10,-2),
+//     std::make_pair(-8,-2),
+//     std::make_pair(-6,-2),
+//     std::make_pair(-4,-2)
+// };
+
+// std::vector<std::pair<double,double>> blue_cones = {
+//     std::make_pair(-4, 0),
+//     std::make_pair(-4, 2),
+//     std::make_pair(-4, 4),
+//     std::make_pair(-4, 6),
+//     std::make_pair(-4, 8),
+//     std::make_pair(-4, 10),
+//     std::make_pair(-4, 12),
+//     std::make_pair(-4, 14),
+//     std::make_pair(-4, 16),
+//     std::make_pair(-4, 18),
+//     std::make_pair(-4, 20),
+//     std::make_pair(-4, 22),
+//     std::make_pair(-4-2 + 2 * cos(ator(30)), 22 + 2 * sin(ator(30))),
+//     std::make_pair(-4-2 + 2 * cos(ator(60)), 22 + 2 * sin(ator(60))),
+//     std::make_pair(-6, 24),
+//     std::make_pair(-8, 24),
+//     std::make_pair(-10, 24),
+//     std::make_pair(-12, 24),
+//     std::make_pair(-14, 24),
+//     std::make_pair(-14 + 2 * cos(ator(120)), 24 - 2 + 2 * sin(ator(120))),
+//     std::make_pair(-14 + 2 * cos(ator(150)), 24 - 2 + 2 * sin(ator(150))),
+//     std::make_pair(-16, 22),
+//     std::make_pair(-16 + ftom(4), 20),
+//     std::make_pair(-16 + ftom(6), 18),
+//     std::make_pair(-16 + ftom(2), 16),
+//     std::make_pair(-16 - ftom(2), 14),
+//     std::make_pair(-16 - ftom(6), 12),
+//     std::make_pair(-16 - ftom(4), 10),
+//     std::make_pair(-16, 8),
+//     std::make_pair(-16, 6),
+//     std::make_pair(-16, 4),
+//     std::make_pair(-16+2 - 2*cos(ator(30)),4-2*sin(ator(30))),
+//     std::make_pair(-16+2 - 2*cos(ator(60)),4-2*sin(ator(90))),
+//     std::make_pair(-14,2),
+//     std::make_pair(-12,2),
+//     std::make_pair(-10,2),
+//     std::make_pair(-8,2),
+//     std::make_pair(-6,2),
+//     std::make_pair(-4,2)
+// };
+
 std::vector<std::pair<double,double>> yellow_cones = {
     std::make_pair(0, 0),
-    std::make_pair(0, 2),
-    std::make_pair(0, 4),
-    std::make_pair(0, 6),
-    std::make_pair(0, 8),
-    std::make_pair(0, 10),
-    std::make_pair(0, 12),
-    std::make_pair(0, 14),
-    std::make_pair(0, 16),
-    std::make_pair(0, 18),
-    std::make_pair(0, 20),
-    std::make_pair(0, 22),
-    std::make_pair(0 - 6 + 6 * cos(ator(30)), 22 + 6 * sin(ator(30))),
-    std::make_pair(0 - 6 + 6 * cos(ator(60)), 22 + 6 * sin(ator(60))),
-    std::make_pair(-6, 28),
-    std::make_pair(-8, 28),
-    std::make_pair(-10, 28),
-    std::make_pair(-12, 28),
-    std::make_pair(-14, 28),
-    std::make_pair(-14 + 6 * cos(ator(120)), 28 - 6 + 6 * sin(ator(120))),
-    std::make_pair(-14 + 6 * cos(ator(150)), 28 - 6 + 6 * sin(ator(150))),
-    std::make_pair(-20, 22),
-    std::make_pair(-20 + ftom(4), 20),
-    std::make_pair(-20 + ftom(6), 18),
-    std::make_pair(-20 + ftom(2), 16),
-    std::make_pair(-20 - ftom(2), 14),
-    std::make_pair(-20 - ftom(6), 12),
-    std::make_pair(-20 - ftom(4), 10),
-    std::make_pair(-20, 8),
-    std::make_pair(-20, 6),
-    std::make_pair(-20, 4),
-    std::make_pair(-16+2 - 6*cos(ator(30)),4-6*sin(ator(30))),
-    std::make_pair(-16+2 - 6*cos(ator(60)),4-6*sin(ator(90))),
-    std::make_pair(-14,-2),
-    std::make_pair(-12,-2),
-    std::make_pair(-10,-2),
-    std::make_pair(-8,-2),
-    std::make_pair(-6,-2),
-    std::make_pair(-4,-2)
+    std::make_pair(20, 0),
+    std::make_pair(40, 40),
+    std::make_pair(60, 40)
 };
 
 std::vector<std::pair<double,double>> blue_cones = {
-    std::make_pair(-4, 0),
-    std::make_pair(-4, 2),
-    std::make_pair(-4, 4),
-    std::make_pair(-4, 6),
-    std::make_pair(-4, 8),
-    std::make_pair(-4, 10),
-    std::make_pair(-4, 12),
-    std::make_pair(-4, 14),
-    std::make_pair(-4, 16),
-    std::make_pair(-4, 18),
-    std::make_pair(-4, 20),
-    std::make_pair(-4, 22),
-    std::make_pair(-4-2 + 2 * cos(ator(30)), 22 + 2 * sin(ator(30))),
-    std::make_pair(-4-2 + 2 * cos(ator(60)), 22 + 2 * sin(ator(60))),
-    std::make_pair(-6, 24),
-    std::make_pair(-8, 24),
-    std::make_pair(-10, 24),
-    std::make_pair(-12, 24),
-    std::make_pair(-14, 24),
-    std::make_pair(-14 + 2 * cos(ator(120)), 24 - 2 + 2 * sin(ator(120))),
-    std::make_pair(-14 + 2 * cos(ator(150)), 24 - 2 + 2 * sin(ator(150))),
-    std::make_pair(-16, 22),
-    std::make_pair(-16 + ftom(4), 20),
-    std::make_pair(-16 + ftom(6), 18),
-    std::make_pair(-16 + ftom(2), 16),
-    std::make_pair(-16 - ftom(2), 14),
-    std::make_pair(-16 - ftom(6), 12),
-    std::make_pair(-16 - ftom(4), 10),
-    std::make_pair(-16, 8),
-    std::make_pair(-16, 6),
-    std::make_pair(-16, 4),
-    std::make_pair(-16+2 - 2*cos(ator(30)),4-2*sin(ator(30))),
-    std::make_pair(-16+2 - 2*cos(ator(60)),4-2*sin(ator(90))),
-    std::make_pair(-14,2),
-    std::make_pair(-12,2),
-    std::make_pair(-10,2),
-    std::make_pair(-8,2),
-    std::make_pair(-6,2),
-    std::make_pair(-4,2)
+    std::make_pair(0, 0),
+    std::make_pair(20, 0),
+    std::make_pair(40, 40),
+    std::make_pair(60, 40)
 };
 
 void testMakeSplinesVector() {
@@ -163,40 +177,32 @@ int main() {
     //     std::cout << "FIRST CHUNK IS NULL" << std::endl;
     // }
 
-    // double xBlue = chunks[0]->bluePoints[0].first;
-    // std::pair<double, double> xyYellow = chunks[0]->yellowPoints
-    
-    // std::cout << "CHUNK" << std::endl;
-    // std::cout << chunks[0]->avgCurvature << std::endl;
-    // std::cout << xBlue << std::endl;
+    // outputting chunks
+    // std::ofstream Blue("blue_points.txt");
+    // std::ofstream Yellow("yellow_points.txt");
 
-    std::ofstream Blue("blue_points.txt");
-    std::ofstream Yellow("yellow_points.txt");
+    // for (int i  = 0; i < chunks.size(); i++) {
+    //     // std::cout << "start, end: " << chunks[i]->startProdgress << ", " << chunks[i]->endProgress << std::endl;
+    //     // std::cout << "average curvature: " << chunks[i]->avgCurvature << std::endl;
 
-    for (int i  = 0; i < chunks.size(); i++) {
-        // std::cout << "start, end: " << chunks[i]->startProdgress << ", " << chunks[i]->endProgress << std::endl;
-        // std::cout << "average curvature: " << chunks[i]->avgCurvature << std::endl;
+    //     for (int j = 0; j < chunks[i]->yellowPoints.size(); j++) {
+    //         Blue << chunks[i]->bluePoints[j].first << "," << chunks[i]->bluePoints[j].second << std::endl;
+    //     }
 
-        for (int j = 0; j < chunks[i]->yellowPoints.size(); j++) {
-            Blue << chunks[i]->bluePoints[j].first << "," << chunks[i]->bluePoints[j].second << std::endl;
-        }
+    //     for (int j = 0; j < chunks[i]->yellowPoints.size(); j++) {
+    //         Yellow << chunks[i]->yellowPoints[j].first << "," << chunks[i]->yellowPoints[j].second << std::endl;
+    //     }
 
-        for (int j = 0; j < chunks[i]->yellowPoints.size(); j++) {
-            Yellow << chunks[i]->yellowPoints[j].first << "," << chunks[i]->yellowPoints[j].second << std::endl;
-        }
+    //     Blue << "#";
+    //     Yellow << "#";
+    // }
 
-        // std::cout << "CHUNK " << i << std::endl;
-        // std::cout << "start, end: " << chunks[i]->startProdgress << ", " << chunks[i]->endProgress << std::endl;
-        // std::cout << "average curvature: " << chunks[i]->avgCurvature << std::endl;
-        // for (int j = 0; j < chunks[i]->bluePoints.size(); j++) {
-        //     std::cout << "(" << chunks[i]->bluePoints[j].first << ", " << chunks[i]->bluePoints[j].second << ")" << std::endl;
-        // }
-        Blue << "#";
-        Yellow << "#";
+    // Blue.close();
+    // Yellow.close();
+
+    for (int i = 0; i < chunks[0]->bluePoints.size(); i++) {
+        std::cout << chunks[0]->bluePoints[0].first << "," << chunks[i]->bluePoints[0].second << std::endl;
     }
-
-    Blue.close();
-    Yellow.close();
     
     return 0;
 }
