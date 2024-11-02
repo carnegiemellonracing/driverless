c83a22a05065b924cb67de56b117ec5813951e38
vichan7 <victorih@andrew.cmu.edu>
2024-11-02 14:58:58 -0400

    prints

--- driverless_ws/src/planning/src/planning_codebase/raceline/raceline.cpp
+++ driverless_ws/src/planning/src/planning_codebase/raceline/raceline.cpp
@@ -431,7 +431,7 @@ double arclength(polynomial poly_der1, double x0,double x1){
     gsl_function F;
     F.function = &arclength_f;
     F.params = &poly_der1;
-    std::cout << "deriv: " << poly_der1.nums << std::endl;
+    // std::cout << "deriv: " << poly_der1.nums << std::endl;
 
     double result, error;
     size_t neval;
