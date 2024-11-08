98c784bc5ea3a671adc0ec7c796a444e1acefdfd
vichan7 <victorih@andrew.cmu.edu>
2024-11-08 17:59:14 -0500

    test

--- driverless_ws/src/planning/src/planning_codebase/raceline/raceline.cpp
+++ driverless_ws/src/planning/src/planning_codebase/raceline/raceline.cpp
@@ -165,6 +165,7 @@ std::tuple<Eigen::VectorXd,double, Eigen::VectorXd,double> Spline::along(double 
 
 
     double first_x = this->get_rotated_points()(0,0);
+    std::cout << "first x: " << first_x << std::endl;
     double last_x = this->get_rotated_points()(0,this->get_rotated_points().cols()-1);
 
     //std::cout << "SPLINE ALONG 1" << std::endl;
