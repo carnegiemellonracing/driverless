0a9be5c598c940e9df3de96703bd84a190c3f2bc
Anthony Yip <anthony.ch.yip@gmail.com>
2024-11-15 00:12:53 -0500

    Made models OO, found sync point, quaternion transformation (no interpolation yet)

--- driverless_ws/src/controls/src/dynamics_python/slipless_python.py
+++ driverless_ws/src/controls/src/dynamics_python/slipless_python.py
@@ -5,91 +5,103 @@ import numpy as np
 def clamp(value, min_value, max_value):
     return max(min(value, max_value), min_value)
 
-def kinematic_swangle(speed, swangle):
-    return swangle / (1 + understeer_slope * speed)
 
-def angular_speed(speed, kinematic_swangle_):
-    if kinematic_swangle_ == 0:
-        return 0
+class Slipless:
+    def __init__(self, name="slipless", car_mass=car_mass_default, rolling_drag=rolling_drag_default, long_tractive_capability=long_tractive_capability_default, understeer_slope=understeer_slope_default):
+        self.name = name
+        self.car_mass = car_mass
+        self.rolling_drag = rolling_drag
+        self.long_tractive_capability = long_tractive_capability
+        self.understeer_slope = understeer_slope
+        self.saturating_motor_torque = (self.long_tractive_capability + self.rolling_drag / self.car_mass) * self.car_mass * whl_radius / gear_ratio
 
-    back_whl_to_center_of_rot = (cg_to_front + cg_to_rear) / math.tan(kinematic_swangle_)
-    return math.copysign(
-        speed / math.sqrt(cg_to_rear**2 + back_whl_to_center_of_rot**2),
-        kinematic_swangle_
-    )
 
-def centripedal_accel(speed, swangle):
-    if swangle == 0:
-        return 0
+    def kinematic_swangle(self, speed, swangle):
+        return swangle / (1 + self.understeer_slope * speed)
 
-    kinematic_swangle_ = kinematic_swangle(speed, swangle)
-    back_whl_to_center_of_rot = (cg_to_front + cg_to_rear) / math.tan(kinematic_swangle_)
-    return math.copysign(
-        speed**2 / math.sqrt(cg_to_rear**2 + back_whl_to_center_of_rot**2),
-        kinematic_swangle_
-    )
+    def angular_speed(self, speed, kinematic_swangle_):
+        if kinematic_swangle_ == 0:
+            return 0
 
-def slip_angle(kinematic_swangle):
-    return math.atan(cg_to_rear / (cg_to_front + cg_to_rear) * math.tan(kinematic_swangle))
+        back_whl_to_center_of_rot = (cg_to_front + cg_to_rear) / math.tan(kinematic_swangle_)
+        return math.copysign(
+            speed / math.sqrt(cg_to_rear**2 + back_whl_to_center_of_rot**2),
+            kinematic_swangle_
+        )
 
-def dynamics(state, action):
-    x, y, yaw, speed = state[:slipless_state_dim]
+    def centripedal_accel(self, speed, swangle):
+        if swangle == 0:
+            return 0
 
-    swangle = action[0]
-    torque = action[1] * gear_ratio
+        kinematic_swangle_ = self.kinematic_swangle(speed, swangle)
+        back_whl_to_center_of_rot = (cg_to_front + cg_to_rear) / math.tan(kinematic_swangle_)
+        return math.copysign(
+            speed**2 / math.sqrt(cg_to_rear**2 + back_whl_to_center_of_rot**2),
+            kinematic_swangle_
+        )
 
-    kinematic_swangle_ = kinematic_swangle(speed, swangle)
-    slip_angle_ = slip_angle(kinematic_swangle_)
+    def slip_angle(self, kinematic_swangle):
+        return math.atan(cg_to_rear / (cg_to_front + cg_to_rear) * math.tan(kinematic_swangle))
 
-    saturating_tire_torque = saturating_motor_torque * 0.5 * gear_ratio
+    def dynamics(self, state, action):
+        x, y, yaw, speed = state[:slipless_state_dim]
 
-    if torque_mode == TorqueMode.AWD:
-        torque_front = clamp(torque * 0.5, -saturating_tire_torque, saturating_tire_torque)
-        torque_rear = clamp(torque * 0.5, -saturating_tire_torque, saturating_tire_torque)
-    elif torque_mode == TorqueMode.FWD:
-        torque_front = clamp(torque, -saturating_tire_torque, saturating_tire_torque)
-        torque_rear = 0
-    elif torque_mode == TorqueMode.RWD:
-        torque_front = 0
-        torque_rear = clamp(torque, -saturating_tire_torque, saturating_tire_torque)
+        swangle = action[0]
+        torque = action[1] * gear_ratio
 
-    next_speed_raw = speed + (
-        (torque_front * math.cos(swangle - slip_angle_) + torque_rear * math.cos(slip_angle_)) / 
-        (whl_radius * car_mass) - rolling_drag / car_mass
-    ) * timestep
-    next_speed = max(0.0, next_speed_raw)
+        kinematic_swangle_ = self.kinematic_swangle(speed, swangle)
+        slip_angle_ = self.slip_angle(kinematic_swangle_)
 
-    speed2 = speed**2
-    next_speed2 = next_speed**2
-    dist_avg_speed = (
-        2.0/3.0 * (next_speed2 * next_speed - speed2 * speed) / (next_speed2 - speed2) 
-        if speed != next_speed else speed
-    )
+        saturating_tire_torque = self.saturating_motor_torque * 0.5 * gear_ratio
 
-    kinematic_swangle_ = kinematic_swangle(dist_avg_speed, swangle)
-    angular_speed_ = angular_speed(dist_avg_speed, kinematic_swangle_)
-    next_yaw = yaw + angular_speed_ * timestep
+        if torque_mode == TorqueMode.AWD:
+            torque_front = clamp(torque * 0.5, -saturating_tire_torque, saturating_tire_torque)
+            torque_rear = clamp(torque * 0.5, -saturating_tire_torque, saturating_tire_torque)
+        elif torque_mode == TorqueMode.FWD:
+            torque_front = clamp(torque, -saturating_tire_torque, saturating_tire_torque)
+            torque_rear = 0
+        elif torque_mode == TorqueMode.RWD:
+            torque_front = 0
+            torque_rear = clamp(torque, -saturating_tire_torque, saturating_tire_torque)
 
-    rear_to_center = (cg_to_rear + cg_to_front) / math.tan(kinematic_swangle_) +  1e-10
-    next_x = (
-        x + dist_avg_speed * math.cos(yaw) * timestep if angular_speed_ == 0
-        else x + cg_to_rear * (math.cos(next_yaw) - math.cos(yaw)) + 
-        rear_to_center * (math.sin(next_yaw) - math.sin(yaw))
-    )
-    next_y = (
-        y + dist_avg_speed * math.sin(yaw) * timestep if angular_speed_ == 0
-        else y + cg_to_rear * (math.sin(next_yaw) - math.sin(yaw)) +
-        rear_to_center * (-math.cos(next_yaw) + math.cos(yaw))
-    )
+        next_speed_raw = speed + (
+            (torque_front * math.cos(swangle - slip_angle_) + torque_rear * math.cos(slip_angle_)) / 
+            (whl_radius * self.car_mass) - self.rolling_drag / self.car_mass
+        ) * timestep
+        next_speed = max(0.0, next_speed_raw)
 
-    next_state = np.array([next_x, next_y, next_yaw, next_speed])
-    return next_state
+        speed2 = speed**2
+        next_speed2 = next_speed**2
+        dist_avg_speed = (
+            2.0/3.0 * (next_speed2 * next_speed - speed2 * speed) / (next_speed2 - speed2) 
+            if speed != next_speed else speed
+        )
+
+        kinematic_swangle_ = self.kinematic_swangle(dist_avg_speed, swangle)
+        angular_speed_ = self.angular_speed(dist_avg_speed, kinematic_swangle_)
+        next_yaw = yaw + angular_speed_ * timestep
+
+        rear_to_center = (cg_to_rear + cg_to_front) / math.tan(kinematic_swangle_) +  1e-10
+        next_x = (
+            x + dist_avg_speed * math.cos(yaw) * timestep if angular_speed_ == 0
+            else x + cg_to_rear * (math.cos(next_yaw) - math.cos(yaw)) + 
+            rear_to_center * (math.sin(next_yaw) - math.sin(yaw))
+        )
+        next_y = (
+            y + dist_avg_speed * math.sin(yaw) * timestep if angular_speed_ == 0
+            else y + cg_to_rear * (math.sin(next_yaw) - math.sin(yaw)) +
+            rear_to_center * (-math.cos(next_yaw) + math.cos(yaw))
+        )
+
+        next_state = np.array([next_x, next_y, next_yaw, next_speed])
+        return next_state
 
 def main():
-    state = np.array([0.0, 0.0, 0.0, 0.0])
-    action = np.array([-0.01, 8.0])
-    next_state = dynamics(state, action)
-    print(next_state)
+    pass
+    # state = np.array([0.0, 0.0, 0.0, 0.0])
+    # action = np.array([-0.01, 8.0])
+    # next_state = dynamics(state, action)
+    # print(next_state)
 
 if __name__ == "__main__":
     main()
