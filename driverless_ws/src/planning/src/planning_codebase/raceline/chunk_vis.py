aceca63509595a97d4471551e69641eacd8e8b94
dnguyen-23 <dnguyen2@andrew.cmu.edu>
2024-11-16 14:08:24 -0500

    chunk_vis.py

--- driverless_ws/src/planning/src/planning_codebase/raceline/chunk_vis.py
+++ driverless_ws/src/planning/src/planning_codebase/raceline/chunk_vis.py
@@ -2,8 +2,6 @@
 After generating the chunks from the chunking algorithm, print out the
 coordinates of the blue and yellow cones into chunk_vis.txt.
 
-Prints out the chunks in alternating colors
-
 Please follow the following format:
 1.) each cone is in the listed in the form: x,y
 2.) End each chunk with a # symbol (including the last)
@@ -31,9 +29,7 @@ cur_chunk_y = np.array([])
 
 for line in lines:
     line = line.strip()
-    if len(lines) == 0 or line == "#":
-        continue
-    elif line != "#" and len(line) > 0:
+    if line != "#" and len(line) > 0:
         comma_idx = line.index(',')
         cur_chunk_x = np.append(cur_chunk_x, [float(line[0:comma_idx])])
         cur_chunk_y = np.append(cur_chunk_y, [float(line[comma_idx+1:])])
@@ -44,37 +40,47 @@ for line in lines:
         cur_chunk_x = np.array([])
         cur_chunk_y = np.array([])
 
+print("DONE READING BLUE CHUNKS")
 plt.style.use('dark_background')
-
-for (l_x, l_y) in zip(blue_chunks_x, blue_chunks_y):
+for (j,(l_x, l_y)) in enumerate(zip(blue_chunks_x, blue_chunks_y)):
     for i in range(len(l_x)):
-        plt.scatter(l_x[i], l_y[i], color='blue')
-
-with open("chunk_vis_yellow.txt") as f:
-    lines = f.readlines() # list containing lines of file
-
-yellow_chunks_x = []
-yellow_chunks_y = []
-cur_chunk_x = np.array([])
-cur_chunk_y = np.array([])
-
-for line in lines:
-    line = line.strip()
-    if len(lines) == 0 or line == "#":
-        continue
-    elif line != "#" and len(line) > 0:
-        comma_idx = line.index(',')
-        cur_chunk_x = np.append(cur_chunk_x, [float(line[0:comma_idx])])
-        cur_chunk_y = np.append(cur_chunk_y, [float(line[comma_idx+1:])])
-    elif line == "#":
-        yellow_chunks_x.append(cur_chunk_x)
-        yellow_chunks_y.append(cur_chunk_y)
-
-        cur_chunk_x = np.array([])
-        cur_chunk_y = np.array([])
-
-    for (l_x, l_y) in zip(yellow_chunks_x, yellow_chunks_y):
-        for i in range(len(l_x)):
-            plt.scatter(l_x[i], l_y[i], color='yellow')
-
+        if j % 2 == 0:
+            print(l_x[i], l_y[i], "BLUE")
+            plt.scatter(l_x[i], l_y[i], color='blue')
+        else:
+            print(l_x[i], l_y[i], "LBLUE")
+            plt.scatter(l_x[i], l_y[i], color='lightskyblue')
 plt.show()
+
+# with open("chunk_vis_yellow.txt") as f:
+#     lines = f.readlines() # list containing lines of file
+#
+# yellow_chunks_x = []
+# yellow_chunks_y = []
+# cur_chunk_x = np.array([])
+# cur_chunk_y = np.array([])
+#
+# for line in lines:
+#     line = line.strip()
+#     if line != "#" and len(line) > 0:
+#         comma_idx = line.index(',')
+#         cur_chunk_x = np.append(cur_chunk_x, [float(line[0:comma_idx])])
+#         cur_chunk_y = np.append(cur_chunk_y, [float(line[comma_idx+1:])])
+#     elif line == "#":
+#         yellow_chunks_x.append(cur_chunk_x)
+#         yellow_chunks_y.append(cur_chunk_y)
+#
+#         cur_chunk_x = np.array([])
+#         cur_chunk_y = np.array([])
+#
+# for (l_x, l_y) in zip(yellow_chunks_x, yellow_chunks_y):
+#     for i in range(len(l_x)):
+#         if i % 2 == 0:
+#             plt.scatter(l_x[i], l_y[i], color='yellow')
+#         else:
+#             plt.scatter(l_x[i], l_y[i], color='red')
+#
+# plt.show()
+
+
+
