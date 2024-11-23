55df909e56a16b665209eacd0ab5222e5c8462b5
RichardZhang06 <richardzhangpt@gmail.com>
2024-11-23 14:48:57 -0500

    working for track 4 only

--- driverless_ws/src/planning/src/testing_framework/my_optimizer.py
+++ driverless_ws/src/planning/src/testing_framework/my_optimizer.py
@@ -15,7 +15,7 @@ def cubic_spline_least_squares(point1, point2):
     return tuple(coefficients)
 
 # interpolate points given a cubic spline
-def interpolate_cubic_spline(coefficients, x_values):
+def interpolate_cubic(coefficients, x_values):
     a, b, c, d = coefficients
     y_values = [a * x**3 + b * x**2 + c * x + d for x in x_values]
     return y_values
@@ -37,6 +37,7 @@ def initial_points(blue_cones, yellow_cones, d1, d2):
     blue_end = blue_cones[-1]
     yellow_end = yellow_cones[-1]
 
+    # first, second, and third points
     points = []
     points.append(((blue_start[0]+yellow_start[0])/2,(blue_start[1]+yellow_start[1])/2))
     
@@ -61,27 +62,69 @@ def run_optimizer(blue_cones_x, blue_cones_y, yellow_cones_x, yellow_cones_y):
 
     # extract initial points
     # 2 and 4 are d1 and d2 respectively
-    points = initial_points(blue_cones,yellow_cones,2,4)
-
+    points = initial_points(blue_cones,yellow_cones,d1=2,d2=4)
+    print(points)
     # test initial points
     # points_x = [point[0] for point in points]
     # points_y = [point[1] for point in points]
     # return [points_x, points_y]
+    
+    # find k and l by taking slope from first and last couple points
+    k = (blue_cones[0][1]-blue_cones[5][1])/(blue_cones[0][0]-blue_cones[5][0])
+    l = (blue_cones[-1][1]-blue_cones[-6][1])/(blue_cones[-1][0]-blue_cones[-6][0])
+    # print(k)
+    # print(l)
 
-    # find cubic splines between initial points
-    spline1 = cubic_spline_least_squares(points[0],points[1])
-    spline2 = cubic_spline_least_squares(points[1],points[2])
+    # set inital points
+    x1,y1 = points[0]
+    x2,y2 = points[1]
+    x3,y3 = points[2]
 
-    # interpolate points on the spline
-    spline1_x = np.linspace(blue_cones[0][0], blue_cones[len(blue_cones)//2][0], num=100)
-    spline1_y = interpolate_cubic_spline(spline1,spline1_x)
-    spline2_x = np.linspace(blue_cones[len(blue_cones)//2][0], blue_cones[-1][0], num=100)
-    spline2_y = interpolate_cubic_spline(spline2,spline2_x)
+    # input matrix for x and y values
+    b_x = np.array([x1,x2,x2,x3,k,l,0,0])
+    b_y = np.array([y1,y2,y2,y3,k,l,0,0])
+    # print(b_x)
+    # print(b_y)
 
-    return [np.append(spline1_x,spline2_x),np.append(spline1_y,spline2_y)]
+    # hardcode inverse matrix
+    A_inverse = np.array([
+        [10, -10, -6, 6, 3, -1, 2, 0.25],
+        [-9, 9, 3, -3, -3.5, 0.5, -1, -0.125],
+        [0, 0, 0, 0, 1, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0],
+        [-6, 6, 10, -10, -1, 3, -2, 0.25],
+        [6, -6, -6, 6, 1, -1, 2, -0.25],
+        [-1.5, 1.5, -1.5, 1.5, -0.25, -0.25, -0.5, 0.0625],
+        [0, 0, 1, 0, 0, 0, 0, 0]
+    ])
 
-# b_x = [1,2,3,4,5]
-# b_y = [4,2,0,2,4]
-# y_x = [1,2,3,4,5]
-# y_y = [0,-2,-4,-2,0]
-# run_optimizer(b_x,b_y,y_x,y_y)
+    # solve large matrix equation
+    X = A_inverse @ b_x
+    Y = A_inverse @ b_y
+
+    # set coefficients
+    a1x, b1x, c1x, d1x, a2x, b2x, c2x, d2x = X
+    a1y, b1y, c1y, d1y, a2y, b2y, c2y, d2y = Y
+
+    # dump coefficients into np array
+    spline_x1 = np.array([a1x,b1x,c1x,d1x])
+    spline_x2 = np.array([a2x,b2x,c2x,d2x])
+    spline_y1 = np.array([a1y,b1y,c1y,d1y])
+    spline_y2 = np.array([a2y,b2y,c2y,d2y])
+
+    # set base values (25 points each)
+    base = np.linspace(0,0.5,num=500)
+
+    # outputs of cubic funtion
+    splinex1 = interpolate_cubic(spline_x1,base)
+    splinex2 = interpolate_cubic(spline_x2,base)
+    spliney1 = interpolate_cubic(spline_y1,base)
+    spliney2 = interpolate_cubic(spline_y2,base)
+
+    # combine the arrays
+    spline_x = np.append(splinex1,splinex2)
+    spline_y = np.append(spliney1,spliney2)
+
+    return [spline_x,spline_y]
+
+    # return [np.array([1,2,3]), np.array([1,2,3])]
