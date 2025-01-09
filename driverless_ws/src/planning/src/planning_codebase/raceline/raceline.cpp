7131b0501d00b8e1885d06881c9f36a6a3f9e3ad
Doris <doris.gohawks@gmail.com>
2025-01-09 14:28:55 -0800

    debug yellow

--- driverless_ws/src/planning/src/planning_codebase/raceline/raceline.cpp
+++ driverless_ws/src/planning/src/planning_codebase/raceline/raceline.cpp
@@ -614,13 +614,12 @@ std::pair<std::vector<ParameterizedSpline>,std::vector<double>> parameterized_sp
             RCLCPP_INFO(logger, "spline y is %f + %fx + %fx^2 + %fx^3\n", spline_y.spl_poly.nums(0), spline_y.spl_poly.nums(1), spline_y.spl_poly.nums(2), spline_y.spl_poly.nums(3));
             // //RCLCPP_INFO(logger, "spline derivative is %f + %fx + %fx^2 + %fx^3\n", spline.first_der.nums(0), spline.first_der.nums(1), spline.first_der.nums(2), spline.first_der.nums(3));
             // TODO rewrite calcLength
-            std::cout << "blue length seg" << arclength(std::make_pair(spline_x.first_der, spline_y.first_der)) << std::endl;
+            std::cout << "blue length seg" << arclength(std::make_pair(spline_x.first_der, spline_y.first_der) 0, 1) << std::endl;
             cumsum.push_back(arclength(std::make_pair(spline_x.first_der, spline_y.first_der), 0, 1));
         } else {
             RCLCPP_INFO(logger, "spline x is %f + %fx + %fx^2 + %fx^3\n", spline_x.spl_poly.nums(0), spline_x.spl_poly.nums(1), spline_x.spl_poly.nums(2), spline_x.spl_poly.nums(3));
             RCLCPP_INFO(logger, "spline y is %f + %fx + %fx^2 + %fx^3\n", spline_y.spl_poly.nums(0), spline_y.spl_poly.nums(1), spline_y.spl_poly.nums(2), spline_y.spl_poly.nums(3));
-            std::cout << "blue length seg" << arclength(std::make_pair(spline_x.first_der, spline_y.first_der)) << std::endl;
-            cumsum.push_back(cumsum.back()+arclength(std::make_pair(spline_x.first_der, spline_y.first_der), 0, 1));
+            std::cout << "blue length seg" << arclength(std::make_pair(spline_x.first_der, spline_y.first_der) 0, 1) << std::endl;            cumsum.push_back(cumsum.back()+arclength(std::make_pair(spline_x.first_der, spline_y.first_der), 0, 1));
         }
         // std::cout << "i: " << i << std::endl;
     }
