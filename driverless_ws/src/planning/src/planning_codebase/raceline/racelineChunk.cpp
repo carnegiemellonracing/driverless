5911015c08811905639ef211ee3ec7c16085ac4b
Doris <doris.gohawks@gmail.com>
2025-01-18 14:32:38 -0500

    yellow length

--- driverless_ws/src/planning/src/planning_codebase/raceline/racelineChunk.cpp
+++ driverless_ws/src/planning/src/planning_codebase/raceline/racelineChunk.cpp
@@ -192,7 +192,7 @@ std::vector<Chunk*>* generateChunks(std::vector<std::pair<double,double>> blueCo
                 // takes in spline, x-y, returns t value
                 // x is the blue percent prog, y is yellow percent prog
                 std::cout << "yellow chunk blue percent length end " << bluePercentProgress * yellowCumulativeLen[yellowCumulativeLen.size() - 1] << std::endl;
-                std::cout << "target length end " << bluePercentProgress * yellowCumulativeLen[yellowCumulativeLen.size() - 1] - yellowStartLen + yellowCumulativeLen[yellowSplineIdx - 1]  << std::endl;
+                std::cout << "target length end " << yellowStartLen - yellowCumulativeLen[yellowSplineIdx - 1]  << std::endl;
 
                 double splitT = ySplit(splitSpline, (bluePercentProgress * yellowCumulativeLen[yellowCumulativeLen.size() - 1]) - yellowStartLen);
                 chunk->tEnd = splitT;
