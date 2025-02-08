2ee4780029a5741cda190b7a367c5d074e8f0888
Anthony Yip <anthony.ch.yip@gmail.com>
2025-02-08 14:39:04 -0500

    Surprisingly easy to change to shared_ptr(?) transitioned Observation and Action to abstract Observation and abstract Action using shared ptr

--- driverless_ws/src/controls/src/learning/environment/environment.hpp
+++ driverless_ws/src/controls/src/learning/environment/environment.hpp
@@ -8,41 +8,46 @@
 using Reward = double;
 
 namespace controls {
-    // An ObservationSpace class implementing a continuous observation (state) space
-    class Observation {
+    // A std::shared_ptr<Action> class implementing an abstract action type
+    class Action {
+        virtual ~Action() = default;
+    };
 
+    // A std::shared_ptr<Observation> class implementing an abstract observation type
+    class Observation {
+        virtual ~Observation() = default;
     };
 
     class ObservationSpace {   
         public:
             ObservationSpace(
-                Observation initialObservation, 
-                Observation (*transitionFunction)(Observation, Action),
-                bool (*isTerminal)(Observation), 
-                bool (*isTruncated)(Observation)
+                std::shared_ptr<Observation> initialObservation, 
+                std::shared_ptr<Observation> (*transitionFunction)(std::shared_ptr<Observation>, Action),
+                bool (*isTerminal)(std::shared_ptr<Observation>), 
+                bool (*isTruncated)(std::shared_ptr<Observation>)
             );
 
-            Observation observe() {
+            std::shared_ptr<Observation> observe() {
                 return this->currState;
             };
 
-            std::tuple<Observation, bool, bool> step(Action a);
+            std::tuple<std::shared_ptr<Observation>, bool, bool> step(Action a);
 
             // Resets the ObservationSpace, returning an initial observation
-            Observation reset();
+            std::shared_ptr<Observation> reset();
             
-            // Returns [true] if [Observation o] is terminal
-            bool isTerminalObservation(Observation o);
+            // Returns [true] if [std::shared_ptr<Observation> o] is terminal
+            bool isTerminalObservation(std::shared_ptr<Observation> o);
 
-            // Returns [true] if [Observation o] has been truncated
-            bool isTruncatedObservation(Observation o);
+            // Returns [true] if [std::shared_ptr<Observation> o] has been truncated
+            bool isTruncatedObservation(std::shared_ptr<Observation> o);
 
         private:
-            Observation initialState;
-            Observation currState;
-            Observation (*transitionFunction)(Observation, Action);
-            bool (*isTerminal)(Observation);
-            bool (*isTruncated)(Observation);
+            std::shared_ptr<Observation> initialState;
+            std::shared_ptr<Observation> currState;
+            std::shared_ptr<Observation> (*transitionFunction)(std::shared_ptr<Observation>, Action);
+            bool (*isTerminal)(std::shared_ptr<Observation>);
+            bool (*isTruncated)(std::shared_ptr<Observation>);
     };
 
 
@@ -50,11 +55,11 @@ namespace controls {
     // Utilizes function pointers over std::function for performance
     class RewardFunction {
         public:
-            RewardFunction(Reward (*rewardFunction)(Observation, Action, Observation));
-            Reward getRewards(Observation s_curr, Action a, Observation s_next);
+            RewardFunction(Reward (*rewardFunction)(std::shared_ptr<Observation>, Action, std::shared_ptr<Observation>));
+            Reward getRewards(std::shared_ptr<Observation> s_curr, Action a, std::shared_ptr<Observation> s_next);
 
         private:
-            Reward (*rewardFunction)(Observation, Action, Observation);
+            Reward (*rewardFunction)(std::shared_ptr<Observation>, Action, std::shared_ptr<Observation>);
     };
 
     class Environment {
@@ -62,21 +67,21 @@ namespace controls {
             std::unordered_map<std::string, std::string> info;
 
             Environment(
-                Observation initialState,
-                Observation (*transitionFunction)(Observation, Action),
-                bool (*isTerminal)(Observation), 
-                bool (*isTruncated)(Observation),
-                Reward (*rewardFunction)(Observation, Action, Observation)
+                std::shared_ptr<Observation> initialState,
+                std::shared_ptr<Observation> (*transitionFunction)(std::shared_ptr<Observation>, Action),
+                bool (*isTerminal)(std::shared_ptr<Observation>), 
+                bool (*isTruncated)(std::shared_ptr<Observation>),
+                Reward (*rewardFunction)(std::shared_ptr<Observation>, Action, std::shared_ptr<Observation>)
             ); 
             // ===== Environment Methods =====
             // Steps the ObservationSpace and returns a tuple of <s', R, truncatedState, terminalState>
-            std::tuple<Observation, Reward, bool, bool> step(Action a); 
+            std::tuple<std::shared_ptr<Observation>, Reward, bool, bool> step(Action a); 
 
             // Invokes the rewardFunction, returing Reward for <s, a, s'> tuple.
-            Reward getRewards(Observation s_curr, Action a, Observation s_next);
+            Reward getRewards(std::shared_ptr<Observation> s_curr, Action a, std::shared_ptr<Observation> s_next);
 
             // Resets the ObservationSpace, returning an initial observation and some info parameter
-            std::pair<Observation, std::unordered_map<std::string, std::string>> reset();
+            std::pair<std::shared_ptr<Observation>, std::unordered_map<std::string, std::string>> reset();
             
             // ===== ObservationSpace setters & getters =====
             void setObservationSpace(ObservationSpace observationSpace);
