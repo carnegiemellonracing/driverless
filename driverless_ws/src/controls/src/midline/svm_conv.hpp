91c9d4eda80d891ece65c1a9d5f7d0f77b30bd5f
Anthony Yip <anthony.ch.yip@gmail.com>
2025-03-29 15:17:37 -0400

    Fixed build errors, drafts for different boundary detection algos implemented

--- driverless_ws/src/controls/src/midline/svm_conv.hpp
+++ driverless_ws/src/controls/src/midline/svm_conv.hpp
@@ -10,8 +10,18 @@ namespace controls {
             conesList cones_to_midline(Cones cones);
         }
 
-        namespace svm_slow  {
+        namespace svm_slow  { 
 
+            conesList cones_to_midline(Cones cones);
+        }
+        namespace svm_strat3{ //binary -> linear if fails
+
+            conesList cones_to_midline(Cones cones);
+        }
+        namespace svm_strat4{ //flood fill
+            conesList cones_to_midline(Cones cones);
+        }
+        namespace svm_naive{ //brute force
             conesList cones_to_midline(Cones cones);
         }
     }
