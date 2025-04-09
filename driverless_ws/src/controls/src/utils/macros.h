b36da786deeb4e86defbfd7358254676de45b799
Anthony Yip <anthony.ch.yip@gmail.com>
2025-04-09 02:34:49 -0400

    renamed the models so they are clearer

--- driverless_ws/src/controls/src/utils/macros.h
+++ driverless_ws/src/controls/src/utils/macros.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <model/slipless/model.cuh>
-#include <model/sysid/model.cuh>
-#include <model/sysid/model_host.h>
-#include <model/slipless/model_host.h>
+#include <model/slipless/slipless_model.cuh>
+#include <model/sysid/sysid_model.cuh>
+#include <model/sysid/sysid_model_host.h>
+#include <model/slipless/slipless_model_host.h>
 
 // ^ I think this is a macro because we didn't want to use a function object
 #define ONLINE_DYNAMICS_FUNC controls::model::sysid::dynamics
