8214a278f160a06e7dd5979940aa0a1c7e64d3d5
Anthony Yip <anthony.ch.yip@gmail.com>
2025-04-13 16:35:18 -0400

    made display being enabled and sysid model being used the default

--- driverless_ws/build_controls.py
+++ driverless_ws/build_controls.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Build controls")
     parser.add_argument("-a", "--asserts", action="store_true")
     parser.add_argument("-d", "--debug", action="store_true")
-    parser.add_argument("-D", "--display", action="store_true")
     parser.add_argument("-c", "--data_collection", action="store_true")
     parser.add_argument("-e", "--export", action="store_true", help="export compile commands")
-    parser.add_argument("-s", "--use_sysid", action="store_true")
     parser.add_argument("-r", "--rosbag", action="store_true")
+    parser.add_argument("--no_sysid_model", action="store_true")
+    parser.add_argument("--no_display", action="store_true")
 
     args = parser.parse_args()
     command = "colcon build --packages-up-to controls --cmake-args"
@@ -19,13 +19,13 @@ if __name__ == "__main__":
         command += " -DPARANOID=ON"
     if args.debug:
         command += " -DCMAKE_BUILD_TYPE=Debug"
-    if args.display:
+    if not args.no_display:
         command += " -DDISPLAY=ON"
     if args.export:
         command += " -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
     if args.data_collection:
         command += " -DDATA=ON"
-    if args.use_sysid:
+    if not args.no_sysid_model:
         command += " -DUSESYSID=ON"
     if args.rosbag:
         command += " -DROSBAG=ON"
