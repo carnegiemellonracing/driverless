28f5b0353880917b13900f93e6243cc4fc31f55a
ChipTheCincoze <geoffthejetson@gmail.com>
2025-04-29 20:52:58 -0400

    tuning constants, high signal to noise ratio

--- driverless_ws/src/controls/src/constants.hpp
+++ driverless_ws/src/controls/src/constants.hpp
@@ -69,8 +69,8 @@ constexpr bool print_svm_timing = false;
 
     constexpr float torque_1Nps_cost = 0.0f;
     constexpr float swangle_1radps_cost = 0.0f;
-        // DEPRECATED
-    constexpr float offset_1m_cost = 20.0f; ///< Cost for being 1m away from midline DEPRECATED
+        
+    constexpr float offset_1m_cost = 5.0f; ///< Cost for being 1m away from midline DEPRECATED
     constexpr float target_speed = 5.0f; ///< Linear cost for under target speed, NO cost for above, in m/s
     constexpr float speed_off_1mps_cost = 1.0f; ///< Cost for being 1m/s below target_speed
 
