433ca61f5926bcb0c85223c5e47a31006b3d7e8d
matthewxrao <matthew.rao06@gmail.com>
2025-07-16 09:23:17 -0400

    Created predictor class and shifted file directories. TODO: File descriptions and doxygen

--- driverless_ws/src/point_to_pixel/include/svm.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "cones.hpp"
-#include "../lib/svm_lib.hpp" // this is really shite way to do this
-
-namespace point_to_pixel {
-namespace recoloring {
-    TrackBounds recolor_cones(TrackBounds track_bounds, double C);
-} // namespace recoloring
-} // namespace point_to_pixel_to_pixel
